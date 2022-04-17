4389115819e818058b1fd02c226248a443f5c140
Pheon-Dev <devpheon@gmail.com>
2022-04-17 15:08:50 +0300

    Using C-SPACE for ToggleTerm

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -2,8 +2,7 @@ require"toggleterm".setup {
   on_config_done = nil,
   -- size can be a number or function which is passed the current terminal
   size = 20,
-  open_mapping = [[<c-t>]],
-  -- open_mapping = [[<ESC>]],
+  open_mapping = [[<C-SPACE>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = false,
