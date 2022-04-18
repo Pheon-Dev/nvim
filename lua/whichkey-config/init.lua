ea6cb2951fa9e7d5424a23c4e36f47da7c6f8100
Pheon-Dev <devpheon@gmail.com>
2022-04-19 02:01:14 +0300

    Trimmed WhickKeys

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -26,36 +26,20 @@ local toggle_lazygit = function()
   return lazygit:toggle()
 end
 
--- local toggle_btop = function()
---   local lazygit = Terminal:new({cmd = 'btop', direction = "float"})
---   return lazygit:toggle()
--- end
-
 local mappings = {
   d = {":Dashboard<cr>", "Home"},
   f = {":Telescope find_files<cr>", "Telescope Find Files"},
   r = {":Telescope live_grep<cr>", "Telescope Live Grep"},
   v = {":ToggleTerm size=20 dir=. direction=horizontal<cr>", "Bottom Terminal"},
-  -- c = {":BufferClose!<cr>", "Close Buffer"},
   h = {":Twilight<cr>", "Twilight"},
   q = {":q!<cr>", "Quit"},
   g = {toggle_lazygit, "LazyGit"},
-  -- b = {toggle_btop, "System Performance"},
   z = {":Telescope zoxide list<cr>", "Zoxide"},
   n = {":Telescope neoclip<cr>", "Neoclip"},
   w = {":Telescope bookmarks<cr>", "Bookmarks"},
   c = {":Telescope repo list<cr>", "Repos"},
   b = {":Telescope frecency<cr>", "Frecency"},
-  p = {
-    name = "Packer",
-    s = {":PackerSync<cr>", "Sync"},
-    c = {":PackerCompile<cr>", "Compile"},
-  },
-  -- e = {
-  --   name = "External",
-  --   e = {":lua <Plug>(external-editor)<cr>", "Editor"},
-  --   b = {":lua <Plug>(external-browser)<cr>", "Browser"},
-  -- },
+  p = {":PackerSync<cr>", "Sync"},
   t = {
     name = "Telescope Git",
     t = {toggle_float, "Floating Terminal"},
@@ -71,27 +55,6 @@ local mappings = {
     h = {":split<cr>", "Horizontal Split"},
     j = {"<C-w>t<C-w>H", "Horiz > Vert Alignment"},
     k = {"<C-w>t<C-w>K", "Vert > Horiz Alignment"}
-  },
-  l = {
-    name = "LSP",
-    i = {":LspInfo<cr>", "Connected Language Servers"},
-    k = {"<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help"},
-    K = {"<cmd>Lspsaga hover_doc<cr>", "Hover Commands"},
-    w = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', "Add Workspace Folder"},
-    W = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', "Remove Workspace Folder"},
-    l = {
-      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
-      "List Workspace Folders"
-    },
-    t = {'<cmd>lua vim.lsp.buf.type_definition()<cr>', "Type Definition"},
-    d = {'<cmd>lua vim.lsp.buf.definition()<cr>', "Go To Definition"},
-    D = {'<cmd>lua vim.lsp.buf.declaration()<cr>', "Go To Declaration"},
-    r = {'<cmd>lua vim.lsp.buf.references()<cr>', "References"},
-    R = {'<cmd>Lspsaga rename<cr>', "Rename"},
-    a = {'<cmd>Lspsaga code_action<cr>', "Code Action"},
-    e = {'<cmd>Lspsaga show_line_diagnostics<cr>', "Show Line Diagnostics"},
-    n = {'<cmd>Lspsaga diagnostic_jump_next<cr>', "Go To Next Diagnostic"},
-    N = {'<cmd>Lspsaga diagnostic_jump_prev<cr>', "Go To Previous Diagnostic"}
   },
 }
 
