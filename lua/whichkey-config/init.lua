9bf0727a3413556a5208d45d5a52d74d8a2cfe16
Pheon-Dev <devpheon@gmail.com>
2022-04-24 19:25:30 +0300

    FIX: Cmus not Toggleable

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -36,11 +36,6 @@ local toggle_btop = function()
   return btop:toggle()
 end
 
-local toggle_cmus = function()
-  local cmus = Terminal:new({cmd = 'cmus', direction = "float"})
-  return cmus:toggle()
-end
-
 local toggle_mc = function()
   local mc = Terminal:new({cmd = 'mc', direction = "float"})
   return mc:toggle()
@@ -63,11 +58,9 @@ local mappings = {
     r = {toggle_ranger, "Ranger"},
     b = {toggle_btop, "Btop"},
     m = {toggle_mc, "mc"},
-    c = {toggle_cmus, "cmus"},
   },
   m = {
     name = "Music",
-    m = {toggle_cmus, "cmus"},
     s = {":CmusPlay<cr>", "Start"},
     p = {":CmusCurrent<cr>", "Current"},
     c = {":Cmus<cr>", "Info"},
