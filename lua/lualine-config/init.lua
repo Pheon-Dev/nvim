38a8f648f571cdaf0e237ac8eaa5526c4286d7de
Pheon-Dev <devpheon@gmail.com>
2022-04-25 16:10:15 +0300

    EDIT: Lualine re-edit

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,10 +1,5 @@
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
 local lualine = require('lualine')
 
--- Color table for highlights
--- stylua: ignore
 local colors = {
   bg       = '#202328',
   fg       = '#8aa2f7',
@@ -37,29 +32,14 @@ local conditions = {
 -- Config
 local config = {
   options = {
-    -- Disable sections and component separators
+    icons_enabled = true,
+    globalstatus = true,
     component_separators = '',
-    section_separators = '',
     theme = {
-      -- We are going to use lualine_c an lualine_x as left and
-      -- right section. Both are highlighted by c theme .  So we
-      -- are just setting default looks o statusline
       normal = { c = { fg = colors.fg, bg = colors.bg } },
-      inactive = { c = { fg = colors.fg, bg = colors.bg } },
     },
   },
   sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    -- These will be filled later
-    lualine_c = {},
-    lualine_x = {},
-  },
-  inactive_sections = {
-    -- these are to remove the defaults
     lualine_a = {},
     lualine_b = {},
     lualine_y = {},
@@ -69,12 +49,10 @@ local config = {
   },
 }
 
--- Inserts a component in lualine_c at left section
 local function ins_left(component)
   table.insert(config.sections.lualine_c, component)
 end
 
--- Inserts a component in lualine_x ot right section
 local function ins_right(component)
   table.insert(config.sections.lualine_x, component)
 end
@@ -86,7 +64,6 @@ ins_left {
     return '▊'
   end,
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -111,17 +88,14 @@ ins_left {
     }
     return { fg = mode_color[vim.fn.mode()] }
   end,
-  padding = { left = 0, right = 1 }, -- We don't need space before this
+  padding = { left = 0, right = 1 },
 }
 
 ins_left {
-  -- mode component
-  -- 'mode',
   function()
     return ' 🎲'
   end,
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -146,7 +120,7 @@ ins_left {
     }
     return { fg = mode_color[vim.fn.mode()] }
   end,
-  padding = { right = 1 },
+  padding = { left = 1, right = 1 },
 }
 
 ins_left {
@@ -157,8 +131,7 @@ ins_left {
 
 ins_left {
   'diff',
-  -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+  symbols = { added = ' ', modified = '柳', removed = ' ' },
   diff_color = {
     added = { fg = colors.green },
     modified = { fg = colors.orange },
@@ -174,10 +147,23 @@ ins_left {
 }
 
 ins_left {
-  function()
-    return "%f"
-  end,
+  'filetype',
+  cond = conditions.buffer_not_empty,
+  icon_only = true,
   color = { fg = my_colors.v, gui = 'bold' },
+}
+
+ins_left {
+  'filename',
+  cond = conditions.buffer_not_empty,
+  color = { fg = my_colors.v, gui = 'bold' },
+  file_status = true,
+  path = 1,
+  symbols = {
+    modified = '[+]',
+    readonly = '[-]',
+    unnamed = '[No Name]',
+  },
 }
 
 ins_right {
@@ -192,17 +178,9 @@ ins_right {
 }
 
 ins_right {
-  'filetype',
-  cond = conditions.buffer_not_empty,
-  color = { fg = my_colors.v, gui = 'bold' },
-}
-
-ins_right {
-  -- filesize component
   'filesize',
   cond = conditions.buffer_not_empty,
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -232,7 +210,6 @@ gui = 'bold',
 
 ins_right { 'location',
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -262,7 +239,6 @@ gui = 'bold',
 
 ins_right { 'progress',
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -291,11 +267,40 @@ gui = 'bold',
 }
 
 ins_right {
+  'mode',
+  color = function()
+    local mode_color = {
+      n = my_colors.n,
+      i = my_colors.i,
+      v = my_colors.v,
+      [''] = colors.magenta,
+      V = my_colors.V,
+      c = my_colors.c,
+      no = my_colors.n,
+      s = colors.violet,
+      S = colors.violet,
+      [''] = colors.violet,
+      ic = colors.blue,
+      R = my_colors.R,
+      Rv = my_colors.R,
+      cv = colors.green,
+      ce = colors.green,
+      r = colors.magenta,
+      rm = colors.magenta,
+      ['r?'] = colors.magenta,
+      ['!'] = colors.green,
+      t = colors.green,
+    }
+    return { fg = mode_color[vim.fn.mode()] }
+  end,
+  padding = { left = 1, right = 1 },
+}
+
+ins_right {
   function()
     return '▊'
   end,
   color = function()
-    -- auto change color according to neovims mode
     local mode_color = {
       n = my_colors.n,
       i = my_colors.i,
@@ -323,5 +328,4 @@ ins_right {
   padding = { left = 1 },
 }
 
--- Now don't forget to initialize lualine
 lualine.setup(config)
