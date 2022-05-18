dcc71d4ef50ccaae57d805e17964cd94be95e9ac
Pheon-Dev <devpheon@gmail.com>
2022-05-18 12:33:40 +0300

    EDIT: Toggle Term Shortcut to Back Tick and Status Line Label

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -51,4 +51,5 @@ function _G.set_terminal_keymaps()
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
