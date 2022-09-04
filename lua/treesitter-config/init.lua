545689602543b8c194a5ff17431ae74c68760545
Pheon-Dev <devpheon@gmail.com>
2022-09-04 10:18:16 +0300

    EDT: Commenting

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -5,26 +5,10 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] =
     update_in_insert = true
   })
 require'nvim-treesitter.configs'.setup {
-  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {"html", "javascript", "lua", "typescript", "tsx", "vue"},
 
-  -- Install languages synchronously (only applied to `ensure_installed`)
-  -- sync_install = false,
-
-  -- List of parsers to ignore installing
-  -- ignore_install = { "javascript" },
-
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
   autotag = {enable = true},
