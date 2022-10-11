530545eea5f0fd133951e49163e14f933b99417f
Pheon-Dev <devpheon@gmail.com>
2022-10-11 12:28:02 +0300

    ADD: Lua LSP

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,6 +1,6 @@
 local capability = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require'lspconfig'.tsserver.setup {
+require 'lspconfig'.tsserver.setup {
   capabilities = capability,
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
@@ -11,26 +11,29 @@ require'lspconfig'.tsserver.setup {
     return vim.loop.cwd()
   end,
   on_attach = function(client)
-    require'lsp_signature'.on_attach()
+    require 'lsp_signature'.on_attach()
   end
 }
 
---[[ local lsp_installer = require('nvim-lsp-installer') ]]
---[[ lsp_installer.on_server_ready(function(server) ]]
---[[   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) ]]
---[[]]
---[[   local opts = {capabilities = capabilities} ]]
---[[   if server.name == "sumneko_lua" then ]]
---[[     opts = vim.tbl_deep_extend("force", { ]]
---[[       settings = { ]]
---[[         Lua = { ]]
---[[           runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')}, ]]
---[[           diagnostics = {globals = {'vim'}}, ]]
---[[           workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false}, ]]
---[[           telemetry = {enable = false} ]]
---[[         } ]]
---[[       } ]]
---[[     }, opts) ]]
---[[   end ]]
---[[   server:setup(opts) ]]
---[[ end) ]]
+require 'lspconfig'.sumneko_lua.setup {
+  on_attach = function(client)
+    require 'lsp_signature'.on_attach()
+  end,
+  capabilities = capability,
+
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
