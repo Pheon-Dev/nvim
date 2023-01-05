ac4b830bdcca713444bf58aa6e32a79b32e70d99
Pheon-Dev <devpheon@gmail.com>
2023-01-05 22:04:20 +0300

    fix: telescope theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -226,6 +226,103 @@ return {
 			-- Give window borders a "portal" feel
 			vim.api.nvim_set_hl(0, "PortalBorder", { fg = "#fab387" })
 			vim.api.nvim_set_hl(0, "PortalBorderNone", { fg = "#89b4fa" })
+
+			-- Telescope
+			local normal_hl = vim.api.nvim_get_hl_by_name("Normal", true)
+
+			local purple0 = "#777777"
+			local purple1 = "#292e42"
+			local purple2 = "#282a36"
+			local bg = "#21222c"
+			local red1 = "#ff5555"
+			local green1 = "#50fa7b"
+			local blue2 = "#bd93f9"
+
+			----------------------------------------------------------------------
+			--                              Prompt                              --
+			----------------------------------------------------------------------
+			vim.api.nvim_set_hl(0, "TelescopePromptBorder", {
+				fg = purple2,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePromptNormal", {
+				fg = blue2,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePromptTitle", {
+				fg = bg,
+				bg = blue2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePromptCounter", {
+				fg = blue2,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePromptPrefix", {
+				fg = blue2,
+				bg = purple2,
+			})
+
+			----------------------------------------------------------------------
+			--                              Result                              --
+			----------------------------------------------------------------------
+			vim.api.nvim_set_hl(0, "TelescopeResultsBorder", {
+				fg = purple2,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopeResultsNormal", {
+				fg = normal_hl.foreground,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopeResultsTitle", {
+				--[[ fg = normal_hl.foreground, ]]
+				fg = bg,
+				bg = green1,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopeSelection", {
+				fg = normal_hl.foreground,
+				bg = purple1,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", {
+				fg = purple0,
+				bg = purple1,
+			})
+
+			----------------------------------------------------------------------
+			--                             Preview                              --
+			----------------------------------------------------------------------
+
+			vim.api.nvim_set_hl(0, "TelescopePreviewBorder", {
+				fg = purple1,
+				bg = purple1,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePreviewNormal", {
+				fg = normal_hl.foreground,
+				bg = purple1,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePreviewTitle", {
+				fg = bg,
+				bg = red1,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePreviewLine", {
+				fg = normal_hl.foreground,
+				bg = purple2,
+			})
+
+			vim.api.nvim_set_hl(0, "TelescopePreviewMatch", {
+				fg = normal_hl.foreground,
+				bg = purple1,
+			})
 		end,
 	},
 }
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,132 +1,127 @@
 return {
-  "glepnir/dashboard-nvim",
-  event = "VimEnter",
-  config = function()
-    vim.g.dashboard_default_executive = "telescope"
+	"glepnir/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		vim.g.dashboard_default_executive = "telescope"
 
-    local db = require("dashboard")
+		local db = require("dashboard")
 
-    db.confirm_key = "l"
+		db.confirm_key = "l"
 
-    db.custom_header = {
-      [[ ███████╗  ██╗   ██╗ ███████╗  ██████╗  ███╗   ██╗     ⠀⠀⠀⠀ ███████╗  ███████╗ ██╗   ██╗ ]],
-      [[ ██╔═══██╗ ██║   ██║ ██╔════╝ ██╔═══██╗ ████═╗ ██║          ██╔═══██╗ ██╔════╝ ██║   ██║ ]],
-      [[ ███████╔╝ ████████║ █████╗   ██║   ██║ ██╔███╗██║ ███████╗ ██║   ██║ █████╗   ██║   ██║ ]],
-      [[ ██╔════╝  ██╔═══██║ ██╔══╝   ██║   ██║ ██║╚═████║  ╚═════╝ ██║   ██║ ██╔══╝   ╚██╗ ██╔╝ ]],
-      [[ ██║       ██║   ██║ ███████╗ ╚██████╔╝ ██║  ╚███║         ⠀███████╔╝ ███████╗  ╚████╔╝  ]],
-      [[ ╚═╝       ╚═╝⠀  ╚═╝ ╚══════╝  ╚═════┘  ╚═╝  ⠀ ╚═╝      ⠀    ╚═════╝  ╚══════╝   ╚═══╝   ]],
-    }
+		db.custom_header = {
+			[[ ███████╗  ██╗   ██╗ ███████╗  ██████╗  ███╗   ██╗     ⠀⠀⠀⠀ ███████╗  ███████╗ ██╗   ██╗ ]],
+			[[ ██╔═══██╗ ██║   ██║ ██╔════╝ ██╔═══██╗ ████═╗ ██║          ██╔═══██╗ ██╔════╝ ██║   ██║ ]],
+			[[ ███████╔╝ ████████║ █████╗   ██║   ██║ ██╔███╗██║ ███████╗ ██║   ██║ █████╗   ██║   ██║ ]],
+			[[ ██╔════╝  ██╔═══██║ ██╔══╝   ██║   ██║ ██║╚═████║  ╚═════╝ ██║   ██║ ██╔══╝   ╚██╗ ██╔╝ ]],
+			[[ ██║       ██║   ██║ ███████╗ ╚██████╔╝ ██║  ╚███║         ⠀███████╔╝ ███████╗  ╚████╔╝  ]],
+			[[ ╚═╝       ╚═╝⠀  ╚═╝ ╚══════╝  ╚═════┘  ╚═╝  ⠀ ╚═╝      ⠀    ╚═════╝  ╚══════╝   ╚═══╝   ]],
+		}
 
-    local Terminal = require("toggleterm.terminal").Terminal
+		local Terminal = require("toggleterm.terminal").Terminal
 
-    local toggle_terminal = function()
-      local float = Terminal:new({ direction = "float" })
-      return float:toggle()
-    end
+		local toggle_terminal = function()
+			local float = Terminal:new({ direction = "float" })
+			return float:toggle()
+		end
 
-    local toggle_fm = function()
-      local fm = Terminal:new({ cmd = "fm", direction = "float" })
-      return fm:toggle()
-    end
+		local toggle_fm = function()
+			local fm = Terminal:new({ cmd = "fm", direction = "float" })
+			return fm:toggle()
+		end
 
-    local toggle_gh = function()
-      local gh = Terminal:new({ cmd = "gh-dash", direction = "float" })
-      return gh:toggle()
-    end
+		local toggle_gh = function()
+			local gh = Terminal:new({ cmd = "gh-dash", direction = "float" })
+			return gh:toggle()
+		end
 
-    local toggle_lazygit = function()
-      local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
-      return lazygit:toggle()
-    end
+		local toggle_lazygit = function()
+			local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
+			return lazygit:toggle()
+		end
 
-    db.custom_center = {
-      {
-        icon = "ﯠ ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Harpoon          ",
-        action = "lua require('harpoon.ui').toggle_quick_menu()",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Grapple Tags     ",
-        action = "GrapplePopup tags",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Find File        ",
-        action = "Telescope find_files theme=dropdown initial_mode=insert",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ lir              ",
-        action = "lua require('lir.float').toggle()",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Search Word      ",
-        action = "Telescope live_grep theme=dropdown",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ File Browser     ",
-        action = "Telescope file_browser theme=ivy initial_mode=normal",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ File Manager     ",
-        action = toggle_fm,
-      },
-      {
-        icon = "פּ ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ NeoTree          ",
-        action = "Neotree",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ TODO             ",
-        action = "TodoTelescope keywords=TODO,FIX,BUG,FIXIT,ISSUE,FIXME,ERROR,WARNING,INFO,HINT,TEST,HACK,PERF,NOTE",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Mason            ",
-        action = "Mason",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ PRs              ",
-        action = toggle_gh,
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Lazygit          ",
-        action = toggle_lazygit,
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Termianl         ",
-        action = toggle_terminal,
-      },
-      {
-        icon = "炙",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Help Tags        ",
-        action = "Telescope help_tags theme=dropdown initial_mode=insert",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Startup Time     ",
-        action = "StartupTime",
-      },
-      {
-        icon = " ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Lazy             ",
-        action = "Lazy",
-      },
-      {
-        icon = "✗ ",
-        desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Exit             ",
-        action = "q!",
-      },
-    }
+		db.custom_center = {
+			{
+				icon = "ﯠ ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Harpoon          ",
+				action = "lua require('harpoon.ui').toggle_quick_menu()",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Grapple Tags     ",
+				action = "GrapplePopup tags",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Find File        ",
+				action = "Telescope find_files theme=dropdown initial_mode=insert",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ lir              ",
+				action = "lua require('lir.float').toggle()",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Search Word      ",
+				action = "Telescope live_grep theme=dropdown",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ File Manager     ",
+				action = toggle_fm,
+			},
+			{
+				icon = "פּ ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ NeoTree          ",
+				action = "Neotree",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ TODO             ",
+				action = "TodoTelescope keywords=TODO,FIX,BUG,FIXIT,ISSUE,FIXME,ERROR,WARNING,INFO,HINT,TEST,HACK,PERF,NOTE",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Mason            ",
+				action = "Mason",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ PRs              ",
+				action = toggle_gh,
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Lazygit          ",
+				action = toggle_lazygit,
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Termianl         ",
+				action = toggle_terminal,
+			},
+			{
+				icon = "炙",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Help Tags        ",
+				action = "Telescope help_tags theme=dropdown initial_mode=insert",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Startup Time     ",
+				action = "StartupTime",
+			},
+			{
+				icon = " ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Lazy             ",
+				action = "Lazy",
+			},
+			{
+				icon = "✗ ",
+				desc = " ⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯ Exit             ",
+				action = "q!",
+			},
+		}
 
-    db.custom_footer = {}
-  end,
+		db.custom_footer = {}
+	end,
 }
