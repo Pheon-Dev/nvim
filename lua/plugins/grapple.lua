259a24b366d52e1fbadc5a6b6859a621a0d45256
Pheon-Dev <devpheon@gmail.com>
2023-01-05 11:59:48 +0300

    feat: grapple

---
+++ lua/plugins/grapple.lua
@@ -0,0 +1,76 @@
+return {
+	{
+		"cbochs/grapple.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("grapple").setup({
+				log_level = "warn",
+				scope = "git",
+				popup_options = {
+					relative = "editor",
+					width = 60,
+					height = 12,
+					style = "minimal",
+					focusable = false,
+					border = "single",
+				},
+				integrations = {
+					resession = false,
+				},
+			})
+		end,
+	},
+	--[[ { ]]
+	--[[ 	"cbochs/portal.nvim", ]]
+	--[[ 	dependencies = { ]]
+	--[[ 		"cbochs/grapple.nvim", -- Optional: provides the "grapple" query item ]]
+	--[[ 		"Pheon-Dev/harpoon", -- Optional: provides the "harpoon" query item ]]
+	--[[ 	}, ]]
+	--[[ 	config = function() ]]
+	--[[ 		require("portal").setup({ ]]
+	--[[ 			log_level = "warn", ]]
+	--[[ 			query = { "modified", "different", "valid", "grapple" }, ]]
+	--[[ 			labels = { "j", "k", "h", "l" }, ]]
+	--[[ 			escape = { ]]
+	--[[ 				["<esc>"] = true, ]]
+	--[[ 			}, ]]
+	--[[ 			lookback = 100, ]]
+	--[[ 			portal = { ]]
+	--[[ 				title = { ]]
+	--[[ 					render_empty = true, ]]
+	--[[ 					options = { ]]
+	--[[ 						relative = "cursor", ]]
+	--[[ 						width = 80, ]]
+	--[[ 						height = 1, ]]
+	--[[ 						col = 2, ]]
+	--[[ 						style = "minimal", ]]
+	--[[ 						focusable = false, ]]
+	--[[ 						border = "single", ]]
+	--[[ 						noautocmd = true, ]]
+	--[[ 						zindex = 98, ]]
+	--[[ 					}, ]]
+	--[[ 				}, ]]
+	--[[ 				body = { ]]
+	--[[ 					render_empty = false, ]]
+	--[[ 					options = { ]]
+	--[[ 						relative = "cursor", ]]
+	--[[ 						width = 80, ]]
+	--[[ 						height = 3, ]]
+	--[[ 						col = 2, ]]
+	--[[ 						focusable = false, ]]
+	--[[ 						border = "single", ]]
+	--[[ 						noautocmd = true, ]]
+	--[[ 						zindex = 99, ]]
+	--[[ 					}, ]]
+	--[[ 				}, ]]
+	--[[ 			}, ]]
+	--[[ 		}) ]]
+	--[[]]
+	--[[ 		local query = require("portal.query").resolve({ "grapple" }) ]]
+	--[[ 		local jumps = require("portal.jump").search(query, "backward") ]]
+	--[[ 		require("portal.jump").select(jumps[1]) ]]
+	--[[ 	end, ]]
+	--[[ }, ]]
+}
