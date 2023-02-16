8d1158679d5f01fe8f222121ee84ed0bb12370c7
Pheon-Dev <devpheon@gmail.com>
2023-02-16 12:13:27 +0300

    fix: neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,10 +1,14 @@
 return {
-	"nvim-orgmode/orgmode",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	build = { ":Neorg sync-parsers" },
-	opts = {
-		load = {
-			["core.defaults"] = {},
-		},
-	},
+	"nvim-neorg/neorg",
+	ft = "norg", -- lazy load on filetype
+	cmd = "Neorg", -- lazy load on command, allows you to autocomplete :Neorg regardless of whether it's loaded yet
+	--  (you could also just remove both lazy loading things)
+	priority = 30, -- treesitter is on default priority of 50, neorg should load after it.
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+			},
+		})
+	end,
 }
