6a6184ec3bb0bc257ad1570bfb94cba9864cc047
Pheon-Dev <devpheon@gmail.com>
2023-02-18 18:02:45 +0300

    chore: codeium keymaps

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,19 +3,6 @@ return {
 	"Exafunction/codeium.vim",
 	event = "VeryLazy",
 	config = function()
-		vim.g.codeium_disable_bindings = 1
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-g>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true })
-		vim.keymap.set("i", "<c-;>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true })
-		vim.keymap.set("i", "<c-,>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true })
-		vim.keymap.set("i", "<c-x>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true })
+		-- vim.g.codeium_disable_bindings = 1
 	end,
 }
