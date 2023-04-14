e6a0cdbf848cca980062ccf1ebfd74b1a37ef57d
Pheon-Dev <devpheon@gmail.com>
2023-04-14 12:10:31 +0300

    chore: remove backseat

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,24 +1,6 @@
 -- Remove the `use` here if you're using folke/lazy.nvim.
 return {
 	{
-		"james1236/backseat.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("backseat").setup({
-				-- Alternatively, set the env var $OPENAI_API_KEY by putting "export OPENAI_API_KEY=sk-xxxxx" in your ~/.bashrc
-				-- openai_api_key = "sk-xxxxxxxxxxxxxx", -- Get yours from platform.openai.com/account/api-keys
-				openai_model_id = "gpt-3.5-turbo", --gpt-4 (If you do not have access to a model, it says "The model does not exist")
-
-				split_threshold = 100,
-				additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
-				highlight = {
-					icon = "", -- ''
-					group = "Comment",
-				},
-			})
-		end,
-	},
-	{
 		"Exafunction/codeium.vim",
 		event = "VeryLazy",
 		config = function()
