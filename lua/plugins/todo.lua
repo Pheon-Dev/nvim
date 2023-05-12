3721cf8b327e10ac661de4e370fbb8dc05e7e5e1
Pheon-Dev <devpheon@gmail.com>
2023-05-12 13:33:47 +0300

    ~ todo symbols

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -10,17 +10,17 @@ return {
 			-- keywords recognized as todo comments
 			keywords = {
 				FIX = {
-					icon = " ", -- icon used for the sign, and in search results
+					icon = " ", -- icon used for the sign, and in search results
 					color = "error", -- can be a hex color, or a named color (see below)
 					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
 					-- signs = false, -- configure signs for some keywords individually
 				},
-				TODO = { icon = " ", color = "info" },
-				HACK = { icon = " ", color = "warning" },
-				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
 				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
 			},
 			gui_style = {
 				fg = "NONE", -- The gui style to use for the fg highlight group.
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,4 +1,9 @@
 return {
+	-- {
+	-- 	"kevinhwang91/nvim-bqf",
+	-- 	event = "BufReadPre",
+	-- 	config = true,
+	-- },
 	{
 		"folke/trouble.nvim",
 		event = "BufReadPre",
