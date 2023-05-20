422e7e070078d69dd83db60062071ca0ba57d984
Pheon-Dev <devpheon@gmail.com>
2023-05-20 15:51:16 +0300

    [~] fzf-lua config

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,9 +1,7 @@
 return {
 	{
-		"gennaro-tedesco/nvim-possession",
-		dependencies = {
-			"ibhagwan/fzf-lua",
-		},
+		"ibhagwan/fzf-lua",
+		event = "VeryLazy",
 		config = function()
 			local theme = require("config.colors")
 
@@ -16,7 +14,7 @@ return {
 			vim.api.nvim_set_hl(0, "FzfLuaSearch", { fg = theme.color100, bg = theme.color0 })
 
 			vim.api.nvim_set_hl(0, "FzfLuaHelpNormal", { bg = theme.color0 })
-			vim.api.nvim_set_hl(0, "FzfLuaTitle", { fg = theme.color7, bg = theme.color0 })
+			vim.api.nvim_set_hl(0, "FzfLuaTitle", { fg = theme.color0, bg = theme.color89 })
 
 			vim.api.nvim_set_hl(0, "FzfLuaScrollBorderEmpty", { fg = theme.color2, bg = theme.color0 })
 			vim.api.nvim_set_hl(0, "FzfLuaScrollBorderFull", { fg = theme.color2, bg = theme.color0 })
@@ -28,7 +26,80 @@ return {
 
 			require("fzf-lua").setup({
 				"telescope", -- max-perf, fzf-native, default, fzf-tmux, skim, telescope
+				winopts = {
+					-- split         = "belowright new",-- open in a split instead?
+					-- "belowright new"  : split below
+					-- "aboveleft new"   : split above
+					-- "belowright vnew" : split right
+					-- "aboveleft vnew   : split left
+					-- Only valid when using a float window
+					-- (i.e. when 'split' is not defined, default)
+					height = 0.85, -- window height
+					width = 0.80, -- window width
+					row = 0.35, -- window row position (0=top, 1=bottom)
+					col = 0.50, -- window col position (0=left, 1=right)
+					-- border argument passthrough to nvim_open_win(), also used
+					-- to manually draw the border characters around the preview
+					-- window, can be set to 'false' to remove all borders or to
+					-- 'none', 'single', 'double', 'thicc' or 'rounded' (default)
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					-- requires neovim > v0.9.0, passed as is to `nvim_open_win`
+					-- can be sent individually to any provider to set the win title
+					title = "Possession",
+					title_pos = "center", -- 'left', 'center' or 'right'
+					fullscreen = false, -- start fullscreen?
+					preview = {
+						-- default     = 'bat',           -- override the default previewer?
+						-- default uses the 'builtin' previewer
+						border = "border", -- border|noborder, applies only to
+						-- native fzf previewers (bat/cat/git/etc)
+						wrap = "nowrap", -- wrap|nowrap
+						hidden = "nohidden", -- hidden|nohidden
+						vertical = "down:45%", -- up|down:size
+						horizontal = "right:60%", -- right|left:size
+						layout = "flex", -- horizontal|vertical|flex
+						flip_columns = 120, -- #cols to switch to horizontal on flex
+						-- Only used with the builtin previewer:
+						title = true, -- preview border title (file/buf)?
+						title_align = "left", -- left|center|right, title alignment
+						scrollbar = "float", -- `false` or string:'float|border'
+						-- float:  in-window floating border
+						-- border: in-border chars (see below)
+						scrolloff = "-2", -- float scrollbar offset from right
+						-- applies only when scrollbar = 'float'
+						scrollchars = { "█", "" }, -- scrollbar chars ({ <full>, <empty> }
+						-- applies only when scrollbar = 'border'
+						delay = 100, -- delay(ms) displaying the preview
+						-- prevents lag on fast scrolling
+						winopts = {
+							-- builtin previewer window options
+							number = true,
+							relativenumber = false,
+							cursorline = true,
+							cursorlineopt = "both",
+							cursorcolumn = false,
+							signcolumn = "no",
+							list = false,
+							foldenable = false,
+							foldmethod = "manual",
+						},
+					},
+					on_create = function()
+						-- called once upon creation of the fzf main window
+						-- can be used to add custom fzf-lua mappings, e.g:
+						-- vim.keymap.set("t", "<C-j>", "<Down>", { silent = true, buffer = true })
+					end,
+				},
 			})
+		end,
+	},
+	{
+		"gennaro-tedesco/nvim-possession",
+		event = "VeryLazy",
+		dependencies = {
+			"ibhagwan/fzf-lua",
+		},
+		config = function()
 			require("nvim-possession").setup({
 				sessions = {
 					sessions_path = vim.fn.stdpath("data") .. "/sessions/",
