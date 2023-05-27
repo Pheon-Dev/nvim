cd3d355ab6811c749ada5fd52d1950fc34e1a10d
Pheon-Dev <devpheon@gmail.com>
2023-05-28 00:08:26 +0300

    [~] config tabnine

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -42,7 +42,7 @@ return {
 
 		tabnine:setup({
 			max_lines = 1000,
-			max_num_results = 20,
+			max_num_results = 3,
 			sort = true,
 			run_on_every_keystroke = true,
 			snippet_placeholder = "..",
