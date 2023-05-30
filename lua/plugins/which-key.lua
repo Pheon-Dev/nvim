3385cbd3db60f7015bf09e15a8d7c85e51a22515
Pheon-Dev <devpheon@gmail.com>
2023-05-30 23:24:36 +0300

    [+] Pigoen key bindings [~] formatting

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -35,30 +35,30 @@ return {
       icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
-        group = "+",  -- symbol prepended to a group
+        group = "+",      -- symbol prepended to a group
       },
       popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
       },
       window = {
-        border = "none",      -- none, single, double, shadow
-        position = "bottom",  -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "none",          -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
       },
       layout = {
-        height = { min = 4, max = 25 },                                          -- min and max height of the columns
-        width = { min = 20, max = 50 },                                          -- min and max width of the columns
-        spacing = 3,                                                             -- spacing between columns
-        align = "left",                                                          -- align columns left, center or right
+        height = { min = 4, max = 25 },                                             -- min and max height of the columns
+        width = { min = 20, max = 50 },                                             -- min and max width of the columns
+        spacing = 3,                                                                -- spacing between columns
+        align = "left",                                                             -- align columns left, center or right
       },
-      ignore_missing = false,                                                    -- enable this to hide mappings for which you didn't specify a label
+      ignore_missing = false,                                                       -- enable this to hide mappings for which you didn't specify a label
       hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-      show_help = true,                                                          -- show help message on the command line when the popup is visible
-      show_keys = true,                                                          -- show the currently pressed key and its label as a message in the command line
-      triggers = "auto",                                                         -- automatically setup triggers
+      show_help = true,                                                             -- show help message on the command line when the popup is visible
+      show_keys = true,                                                             -- show the currently pressed key and its label as a message in the command line
+      triggers = "auto",                                                            -- automatically setup triggers
       -- triggers = {"<leader>"} -- or specify a list manually
       triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
@@ -155,7 +155,13 @@ return {
         x = { ":lua require('harpoon.mark').clear_all()<cr>", "Clear All Marks" },
       },
       q = { ":bd<cr>", "Close Buffer" },
-      p = { ":lua require('pigeon').toggle()<cr>", "Toggle Pigeon" },
+      p = {
+        name = "Pigeon",
+        p = { ":PigeonToggle<cr>", "Toggle All Pigeon Modules" },
+        d = { ":PigeonToggleDateTime<cr>", "Toggle Date and Time Module" },
+        b = { ":PigeonToggleBattery<cr>", "Toggle Battery Module" },
+        i = { ":PigeonToggleInternet<cr>", "Toggle Internet Module" },
+      },
       r = {
         name = "Rust",
         a = { rt.code_action_group.code_action_group, "Code Action Group" },
