665686bbe89773546ff5089b2b25bbd4046cdd51
Pheon-Dev <devpheon@gmail.com>
2023-06-02 12:34:41 +0300

    [~] formatting

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,29 +1,29 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
-	},
-	{
-		"xiyaowong/virtcolumn.nvim",
-		event = "BufReadPre",
-	},
-	{
-		"utilyre/sentiment.nvim",
-		event = "BufReadPre",
-		config = function()
-			require("sentiment").setup({})
-		end,
-	},
-	{
-		"mg979/vim-visual-multi",
-		event = "BufReadPre",
-		config = function()
-			-- require("sentiment").setup({})
-		end,
-	},
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+  },
+  {
+    "xiyaowong/virtcolumn.nvim",
+    event = "BufReadPre",
+  },
+  {
+    "utilyre/sentiment.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("sentiment").setup({})
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "BufReadPre",
+    config = function()
+      -- require("sentiment").setup({})
+    end,
+  },
 }
