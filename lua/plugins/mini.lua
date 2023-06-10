81ded6f140585bd3ad8037f805cee822bf5e60a1
Pheon-Dev <devpheon@gmail.com>
2023-06-10 11:15:13 +0300

    [-] splitjoin [-] surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,52 +1,9 @@
 return {
-	-- {
-	-- 	"echasnovski/mini.ai",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("mini.ai").setup({})
-	-- 	end,
-	-- },
-  -- {
-  --   "Wansmer/treesj",
-  --   keys = {
-  --     { "<space>l", "<cmd>TSJSplit<CR>", desc = "Treesitter Split" },
-  --     { "<space>h", "<cmd>TSJJoin<CR>", desc = "Treesitter Join" },
-  --     -- { "<space>g", "<cmd>TSJToggle<CR>", desc = "Treesitter Toggle" },
-  --   },
-  --   config = function()
-  --     require("treesj").setup { use_default_keymaps = false }
-  --   end,
-  -- },
 	{
-		"echasnovski/mini.splitjoin",
+		"echasnovski/mini.ai",
 		event = "VeryLazy",
 		config = function()
-			require("mini.splitjoin").setup({})
+			require("mini.ai").setup({})
 		end,
 	},
-
-	--[[ {
-		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		config = function()
-			require("mini.pairs").setup({})
-		end,
-	}, ]]
-
-  {
-    -- Similar to tpope/vim-surround
-    -- dsf to delete surrounding function call.
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup()
-    end,
-  },
-	-- {
-	-- 	"echasnovski/mini.surround",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("mini.surround").setup({})
-	-- 	end,
-	-- },
 }
