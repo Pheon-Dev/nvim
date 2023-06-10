32ef1c6fab438f35c87c55d4ab4478f707982244
Pheon-Dev <devpheon@gmail.com>
2023-06-10 14:35:13 +0300

    [-] kiyoon

--- lua/plugins/text-objects.lua
+++ lua/plugins/text-objects.lua
@@ -7,8 +7,8 @@ return {
 			require("various-textobjs").setup({
 				lookForwardSmall = 5,
 				lookForwardBig = 15,
-				useDefaultKeymaps = false,
-				disabledKeymaps = { "ai", "ii", "aI", "iI" },
+				useDefaultKeymaps = true,
+				-- disabledKeymaps = { "ai", "ii", "aI", "iI" },
 			})
 		end,
 	},
@@ -30,34 +30,5 @@ return {
 			{ "ac", mode = { "o", "x" }, desc = "Select comment block" },
 		},
 		dependencies = { "kana/vim-textobj-user" },
-	},
-	{
-		"kiyoon/treesitter-indent-object.nvim",
-		keys = {
-			{
-				"ai",
-				"<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>",
-				mode = { "x", "o" },
-				desc = "Select context-aware indent (outer)",
-			},
-			{
-				"aI",
-				"<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>",
-				mode = { "x", "o" },
-				desc = "Select context-aware indent (outer, line-wise)",
-			},
-			{
-				"ii",
-				"<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>",
-				mode = { "x", "o" },
-				desc = "Select context-aware indent (inner, partial range)",
-			},
-			{
-				"iI",
-				"<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>",
-				mode = { "x", "o" },
-				desc = "Select context-aware indent (inner, entire range)",
-			},
-		},
 	},
 }
