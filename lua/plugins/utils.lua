c6ba602ecab25035b7214b44a865bcccc53bef19
Pheon-Dev <devpheon@gmail.com>
2023-06-10 11:54:32 +0300

    [~] formatting

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,59 +1,87 @@
 return {
-	"lewis6991/impatient.nvim",
-	"nathom/filetype.nvim",
-	{
-		"imsnif/kdl.vim",
-		event = "VeryLazy",
-	},
+  "lewis6991/impatient.nvim",
+  "nathom/filetype.nvim",
+  {
+    "imsnif/kdl.vim",
+    event = "VeryLazy",
+  },
 
-	-- measure startuptime
-	{
-		"dstein64/vim-startuptime",
-		cmd = "StartupTime",
-		config = function()
-			vim.g.startuptime_tries = 10
-		end,
-	},
+  -- measure startuptime
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 
-	-- persistence
-	{
-		"folke/persistence.nvim",
-		event = "BufReadPre",
-		config = true,
-	},
-	"nvim-lua/plenary.nvim",
+  -- persistence
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = true,
+  },
+  "nvim-lua/plenary.nvim",
 
-	"Pheon-Dev/harpoon",
-	-- "ThePrimeagen/harpoon",
-	{
-		"booperlv/nvim-gomove",
-		event = "BufReadPre",
-		config = function()
-			require("gomove").setup({
-				-- whether or not to map default key bindings, (true/false)
-				map_defaults = false,
-				-- whether or not to reindent lines moved vertically (true/false)
-				reindent = true,
-				-- whether or not to undojoin same direction moves (true/false)
-				undojoin = true,
-				-- whether to not to move past end column when moving blocks horizontally, (true/false)
-				move_past_end_col = false,
-			})
-		end,
-	},
-	{ "kevinhwang91/nvim-hlslens", config = true },
+  "Pheon-Dev/harpoon",
+  -- "ThePrimeagen/harpoon",
+  {
+    "booperlv/nvim-gomove",
+    event = "BufReadPre",
+    config = function()
+      require("gomove").setup({
+        -- whether or not to map default key bindings, (true/false)
+        map_defaults = false,
+        -- whether or not to reindent lines moved vertically (true/false)
+        reindent = true,
+        -- whether or not to undojoin same direction moves (true/false)
+        undojoin = true,
+        -- whether to not to move past end column when moving blocks horizontally, (true/false)
+        move_past_end_col = false,
+      })
+    end,
+  },
+  { "kevinhwang91/nvim-hlslens", config = true },
 
-	{ "ckipp01/stylua-nvim" },
+  { "ckipp01/stylua-nvim" },
 
   {
     "Wansmer/treesj",
     keys = {
-      { "gk", "<cmd>TSJSplit<CR>", desc = "Treesitter Split" },
-      { "gj", "<cmd>TSJJoin<CR>", desc = "Treesitter Join" },
-      -- { "<space>g", "<cmd>TSJToggle<CR>", desc = "Treesitter Toggle" },
+      -- { "gk", "<cmd>TSJSplit<CR>", desc = "Treesitter Split" },
+      -- { "gj", "<cmd>TSJJoin<CR>", desc = "Treesitter Join" },
+      { "gj", "<cmd>TSJToggle<CR>", desc = "Treesitter Toggle" },
     },
     config = function()
-      require("treesj").setup { use_default_keymaps = false }
+      local tsj = require('treesj')
+
+      local langs = { --[[ configuration for languages ]] }
+
+      tsj.setup({
+        -- Use default keymaps
+        -- (<space>m - toggle, <space>j - join, <space>s - split)
+        use_default_keymaps = false,
+
+        -- Node with syntax error will not be formatted
+        check_syntax_error = true,
+
+        -- If line after join will be longer than max value,
+        -- node will not be formatted
+        max_join_length = 120,
+
+        -- hold|start|end:
+        -- hold - cursor follows the node/place on which it was called
+        -- start - cursor jumps to the first symbol of the node being formatted
+        -- end - cursor jumps to the last symbol of the node being formatted
+        cursor_behavior = 'hold',
+
+        -- Notify about possible problems or not
+        notify = true,
+        langs = langs,
+
+        -- Use `dot` for repeat action
+        dot_repeat = true,
+      })
     end,
   },
   {
