c1b6251f44f4ea70280b1d158273ceff4f497de2
Pheon-Dev <devpheon@gmail.com>
2023-06-10 15:57:19 +0300

    [+] curly braces text object

--- lua/plugins/text-objects.lua
+++ lua/plugins/text-objects.lua
@@ -1,34 +1,45 @@
 return {
-  {
-    "chrisgrieser/nvim-various-textobjs",
-    event = "BufReadPre",
-    opts = { useDefaultKeymaps = true },
-    config = function()
-      require("various-textobjs").setup({
-        lookForwardSmall = 5,
-        lookForwardBig = 15,
-        useDefaultKeymaps = true,
-        -- disabledKeymaps = { "ai", "ii", "aI", "iI" },
-      })
-    end,
-  },
-  {
-    "kana/vim-textobj-user",
-    event = "BufReadPre",
-    dependencies = {
-      "beloglazov/vim-textobj-quotes",
-      "kana/vim-textobj-entire",
-    },
-    config = function()
-      vim.keymap.set("n", "q", "iq", { noremap = true, silent = true })
-    end,
-  },
-  {
-    "glts/vim-textobj-comment",
-    keys = {
-      { "ic", mode = { "o", "x" }, desc = "Select comment block" },
-      { "ac", mode = { "o", "x" }, desc = "Select comment block" },
-    },
-    dependencies = { "kana/vim-textobj-user" },
-  },
+	{
+		"chrisgrieser/nvim-various-textobjs",
+		event = "BufReadPre",
+		opts = { useDefaultKeymaps = true },
+		config = function()
+			local map = vim.api.nvim_set_keymap
+
+			map("n", "viB", "vi{", { noremap = true, silent = true })
+			map("n", "vaB", "va{", { noremap = true, silent = true })
+			map("n", "yiB", "yi{", { noremap = true, silent = true })
+			map("n", "yaB", "ya{", { noremap = true, silent = true })
+			map("n", "ciB", "ci{", { noremap = true, silent = true })
+			map("n", "caB", "ca{", { noremap = true, silent = true })
+			map("n", "diB", "di{", { noremap = true, silent = true })
+			map("n", "daB", "da{", { noremap = true, silent = true })
+
+			require("various-textobjs").setup({
+				lookForwardSmall = 5,
+				lookForwardBig = 15,
+				useDefaultKeymaps = true,
+				-- disabledKeymaps = { "ai", "ii", "aI", "iI" },
+			})
+		end,
+	},
+	{
+		"kana/vim-textobj-user",
+		event = "BufReadPre",
+		dependencies = {
+			"beloglazov/vim-textobj-quotes",
+			"kana/vim-textobj-entire",
+		},
+		config = function()
+			vim.keymap.set("n", "q", "iq", { noremap = true, silent = true })
+		end,
+	},
+	{
+		"glts/vim-textobj-comment",
+		keys = {
+			{ "ic", mode = { "o", "x" }, desc = "Select comment block" },
+			{ "ac", mode = { "o", "x" }, desc = "Select comment block" },
+		},
+		dependencies = { "kana/vim-textobj-user" },
+	},
 }
