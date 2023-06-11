82a874258db64fafb7b93adf5d0aa08f3b3df034
Pheon-Dev <devpheon@gmail.com>
2023-06-12 00:29:51 +0300

    [-] curly braces mapping [-] q iq

--- lua/plugins/text-objects.lua
+++ lua/plugins/text-objects.lua
@@ -4,17 +4,6 @@ return {
 		event = "BufReadPre",
 		opts = { useDefaultKeymaps = true },
 		config = function()
-			local map = vim.api.nvim_set_keymap
-
-			map("n", "viB", "vi{", { noremap = true, silent = true })
-			map("n", "vaB", "va{", { noremap = true, silent = true })
-			map("n", "yiB", "yi{", { noremap = true, silent = true })
-			map("n", "yaB", "ya{", { noremap = true, silent = true })
-			map("n", "ciB", "ci{", { noremap = true, silent = true })
-			map("n", "caB", "ca{", { noremap = true, silent = true })
-			map("n", "diB", "di{", { noremap = true, silent = true })
-			map("n", "daB", "da{", { noremap = true, silent = true })
-
 			require("various-textobjs").setup({
 				lookForwardSmall = 5,
 				lookForwardBig = 15,
@@ -30,9 +19,6 @@ return {
 			"beloglazov/vim-textobj-quotes",
 			"kana/vim-textobj-entire",
 		},
-		config = function()
-			vim.keymap.set("n", "q", "iq", { noremap = true, silent = true })
-		end,
 	},
 	{
 		"glts/vim-textobj-comment",
