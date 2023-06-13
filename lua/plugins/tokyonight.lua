0395bc339d8f748f65a65dab74d54701e66511fc
Pheon-Dev <devpheon@gmail.com>
2023-06-13 17:21:50 +0300

    [+] alpha foot and info highlights

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -58,7 +58,9 @@ return {
 			vim.api.nvim_set_hl(0, "AlphaDesc", { fg = theme.color20, bg = theme.color0 })
 			vim.api.nvim_set_hl(0, "AlphaIcon", { fg = theme.color100, bg = theme.color0 })
 			vim.api.nvim_set_hl(0, "AlphaQuit", { fg = theme.color16, bg = theme.color0 })
-			vim.api.nvim_set_hl(0, "AlphaFoot", { fg = theme.color2, bg = theme.color0 })
+			vim.api.nvim_set_hl(0, "AlphaFoot", { fg = theme.color3, bg = theme.color0 })
+			vim.api.nvim_set_hl(0, "AlphaInfo", { fg = theme.color68, bg = theme.color0 })
+
 			vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = theme.color1 })
 			vim.api.nvim_set_hl(0, "CodeiumSuggestion", { fg = theme.color3 })
 
