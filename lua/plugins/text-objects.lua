fa535dc4dfe8355680891e9cf63c8c95dc508fa4
Pheon-Dev <devpheon@gmail.com>
2023-06-15 18:43:06 +0300

    [+] text objects

---
+++ lua/plugins/text-objects.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"chrisgrieser/nvim-various-textobjs",
+		event = "BufReadPre",
+		opts = { useDefaultKeymaps = true },
+		config = function()
+			require("various-textobjs").setup({
+				lookForwardSmall = 5,
+				lookForwardBig = 15,
+				useDefaultKeymaps = true,
+				-- disabledKeymaps = { "ai", "ii", "aI", "iI" },
+				disabledKeymaps = {"L"},
+			})
+		end,
+	},
+	{
+		"glts/vim-textobj-comment",
+		keys = {
+			{ "ic", mode = { "o", "x" }, desc = "Select comment block" },
+			{ "ac", mode = { "o", "x" }, desc = "Select comment block" },
+		},
+		dependencies = { "kana/vim-textobj-user" },
+	},
+}
