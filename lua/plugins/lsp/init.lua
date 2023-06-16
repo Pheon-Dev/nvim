4cf22089e24478589b9555d4dcb8840abc69b5b7
Pheon-Dev <devpheon@gmail.com>
2023-06-16 21:48:33 +0300

    [-] lspsaga

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -15,6 +15,7 @@ return {
         config = function()
           require("mason-lspconfig").setup({
             automatic_installation = true,
+            ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
           })
         end,
       },
@@ -95,8 +96,8 @@ return {
           usePlaceholders = true,
         }
       }
-      -- capabilities.offsetEncoding = { "utf-16" }
-      -- require("lspconfig").clangd.setup({ capabilities = capabilities })
+      capabilities.offsetEncoding = { "utf-16" }
+      require("lspconfig").clangd.setup({ capabilities = capabilities })
 
       require("typescript").setup({
         disable_commands = false, -- prevent the plugin from creating Vim commands
@@ -208,46 +209,4 @@ return {
   },
   "onsails/lspkind-nvim",
   "folke/lsp-colors.nvim",
-  {
-    "tami5/lspsaga.nvim",
-    config = function()
-      local saga = require("lspsaga")
-      saga.setup({
-        debug = false,
-        use_saga_diagnostic_sign = true,
-        -- diagnostic sign
-        error_sign = "",
-        warn_sign = "",
-        hint_sign = "",
-        infor_sign = "",
-        diagnostic_header_icon = "   ",
-        code_action_icon = "",
-        code_action_prompt = { enable = true, sign = true, sign_priority = 40, virtual_text = true },
-        finder_definition_icon = "  ",
-        finder_reference_icon = "  ",
-        max_preview_lines = 10,
-        finder_action_keys = {
-          open = "o",
-          vsplit = "s",
-          split = "i",
-          quit = "q",
-          scroll_down = "<C-f>",
-          scroll_up = "<C-b>",
-        },
-        code_action_keys = { quit = "q", exec = "<CR>" },
-        rename_action_keys = { quit = "<C-c>", exec = "<CR>" },
-        definition_preview_icon = "  ",
-        border_style = "single",
-        rename_prompt_prefix = "➤",
-        rename_output_qflist = {
-          enable = false,
-          auto_open_qflist = false,
-        },
-        server_filetype_map = {},
-        diagnostic_prefix_format = "%d. ",
-        diagnostic_message_format = "%m %c",
-        highlight_prefix = false,
-      })
-    end,
-  },
 }
