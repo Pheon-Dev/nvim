c63b986a420e1c94d01f15c8c4a154dd130ad6d1
Pheon-Dev <devpheon@gmail.com>
2023-06-20 01:17:18 +0300

    [-] disable default keymaps

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -11,7 +11,7 @@ return {
     tsj.setup({
       -- Use default keymaps
       -- (<space>m - toggle, <space>j - join, <space>s - split)
-      use_default_keymaps = true,
+      use_default_keymaps = false,
 
       -- Node with syntax error will not be formatted
       check_syntax_error = true,
