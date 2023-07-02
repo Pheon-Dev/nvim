ee89c5e734e00656742d693fefc544529188f85c
Pheon-Dev <devpheon@gmail.com>
2023-07-02 17:22:38 +0300

    [+] lsp config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -9,154 +9,19 @@ return {
     -- event = "BufReadPre",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "onsails/lspkind-nvim",
+      -- "folke/lsp-colors.nvim",
       "mason.nvim",
-      {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-          require("mason-lspconfig").setup({
-            automatic_installation = true,
-            ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
-          })
-        end,
-      },
     },
-    ---@type lspconfig.options
     servers = nil,
     config = function(plugin)
       -- diagnostics
       require("plugins.lsp.diagnostics")
 
       -- lspconfig
-      local capabilities =
-          require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
-
-      ---@type lspconfig.options
-      local servers = plugin.servers or require("plugins.lsp.servers")
-      -- local coq = require("coq")
-      for server, opts in pairs(servers) do
-        local hl_name = "FloatBorder"
-        local border = {
-          { "╭", hl_name },
-          { "─", hl_name },
-          { "╮", hl_name },
-          { "│", hl_name },
-          { "╯", hl_name },
-          { "─", hl_name },
-          { "╰", hl_name },
-          { "│", hl_name },
-        }
-
-        -- LSP settings (for overriding per client)
-        local handlers = {
-          ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-          ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-        }
-
-        opts.handlers = handlers
-        opts.capabilities = capabilities
-        require("lspconfig")[server].setup(opts)
-      end
-      -- setup formatting and keymaps
-      require("util").on_attach(function(client, buffer)
-        require("plugins.lsp.format").on_attach(client, buffer)
-        require("plugins.lsp.keymaps").on_attach(client, buffer)
-      end)
-
-      local nvim_lsp = require('lspconfig')
-      local util = require("lspconfig.util")
-      local on_attach = require("util").on_attach(function(client, buffer)
-        require("plugins.lsp.format").on_attach(client, buffer)
-        require("plugins.lsp.keymaps").on_attach(client, buffer)
-      end)
-      local lsp_flags = {
-        debounce_text_changes = 150,
-      }
-
-      -- GoLang
-      nvim_lsp['gopls'].setup {
-        cmd = { 'gopls' },
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          gopls = {
-            experimentalPostfixCompletions = true,
-            analyses = {
-              unusedparams = true,
-              shadow = true,
-            },
-            staticcheck = true,
-          },
-        },
-        init_options = {
-          usePlaceholders = true,
-        }
-      }
-      capabilities.offsetEncoding = { "utf-16" }
-      require("lspconfig").clangd.setup({ capabilities = capabilities })
-
-      require("typescript").setup({
-        disable_commands = false, -- prevent the plugin from creating Vim commands
-        debug = true,             -- enable debug logging for commands
-        go_to_source_definition = {
-          fallback = true,        -- fall back to standard LSP definition on failure
-        },
-        server = {
-          -- pass options to lspconfig's setup method
-          capabilities = capabilities,
-          on_attach = on_attach,
-          flags = lsp_flags,
-          root_dir = util.root_pattern(".git"),
-          cmd = { "typescript-language-server", "--stdio" },
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "javascript.jsx",
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
-          },
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
-        },
-      })
+      require("plugins.lsp.config").lsp_config(plugin)
     end,
   },
-  -- {
-  -- 	-- url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  -- 	"Pheon-Dev/lsp_lines.nvim",
-  -- 	event = "BufReadPre",
-  -- 	config = function()
-  -- 		require("lsp_lines").register_lsp_virtual_lines()
-  -- 		-- vim.keymap.set("n", "<Leader>xl", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-  -- 	end,
-  -- },
   {
     "jose-elias-alvarez/null-ls.nvim",
     event = "BufReadPre",
@@ -167,16 +32,28 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
-          -- null_ls.builtins.diagnostics.eslint_d,
-          -- null_ls.builtins.diagnostics.eslint,
           null_ls.builtins.completion.spell,
           require("typescript.extensions.null-ls.code-actions"),
         },
       })
     end,
   },
-
-  -- cmdline tools and lsp servers
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true,
+        ensure_installed = {
+          "rust_analyzer",
+          "clangd",
+          "gopls",
+          "bash-language-server",
+          "lua-language-server",
+          "typescript-language-server",
+        },
+      })
+    end,
+  },
   {
     "williamboman/mason.nvim",
     event = "VeryLazy",
@@ -188,7 +65,7 @@ return {
     -- 	"shfmt",
     -- 	"flake8",
     -- },
-    config = function(plugin)
+    config = function()
       require("mason").setup({
         ui = {
           icons = {
@@ -198,15 +75,6 @@ return {
           },
         },
       })
-      -- local mr = require("mason-registry")
-      -- for _, tool in ipairs(plugin.ensure_installed) do
-      --   local p = mr.get_package(tool)
-      --   if not p:is_installed() then
-      --     p:install()
-      --   end
-      -- end
     end,
   },
-  "onsails/lspkind-nvim",
-  "folke/lsp-colors.nvim",
 }
