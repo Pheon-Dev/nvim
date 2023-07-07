840109532f1bbf579ca4b8bbf5194f250a2e097a
Pheon-Dev <devpheon@gmail.com>
2023-07-07 12:30:42 +0300

    [+] tabline [~] statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -53,31 +53,40 @@ return {
     -- Config
     local config = {
       options = {
-        icons_enabled = true,
-        globalstatus = true,
+        icons_enabled = false,
         disabled_filetypes = { statusline = { "alpha" }, winbar = { "alpha" } },
-        component_separators = "",
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+
+        ignore_focus = {}, -- If current filetype is in this list it'll
+        -- always be drawn as inactive statusline
+        -- and the last window will be drawn as active statusline.
+        -- for example if you don't want statusline of
+        -- your file tree / sidebar window to have active
+        -- statusline you can add their filetypes here.
+
+        always_divide_middle = true, -- When set to true, left sections i.e. 'a','b' and 'c'
+        -- can't take over the entire statusline even
+        -- if neither of 'x', 'y' or 'z' are present.
+
+        globalstatus = true, -- enable global statusline (have a single statusline
+        -- at bottom of neovim instead of one for  every window).
+        -- This feature is only available in neovim 0.7 and higher.
+
+        refresh = {          -- sets how often lualine should refresh it's contents (in ms)
+          statusline = 1000, -- The refresh option sets minimum time that lualine tries
+          tabline = 1000,    -- to maintain between refresh. It's not guarantied if situation
+          winbar = 1000      -- arises that lualine needs to refresh itself before this time
+          -- it'll do it.
+
+          -- Also you can force lualine's refresh by calling refresh function
+          -- like require('lualine').refresh()
+        },
         theme = {
           normal = { c = { fg = colors.fg, bg = colors.bg } },
         },
       },
       statusline = {},
-      winbar = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        lualine_c = {},
-        lualine_x = {},
-      },
-      inactive_winbar = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        lualine_c = {},
-        lualine_x = {},
-      },
       sections = {
         lualine_a = {},
         lualine_b = {},
@@ -86,17 +95,26 @@ return {
         lualine_c = {},
         lualine_x = {},
       },
+      tabline = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_y = {},
+        lualine_z = {},
+        lualine_c = {},
+        lualine_x = {},
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_y = {},
+        lualine_z = {},
+        lualine_c = {},
+        lualine_x = {},
+      },
+      inactive_tabline = {},
+      inactive_winbar = {},
+      extensions = {}
     }
-
-    local function wins_left(component)
-      table.insert(config.winbar.lualine_c, component)
-      table.insert(config.inactive_winbar.lualine_c, component)
-    end
-
-    local function wins_right(component)
-      table.insert(config.winbar.lualine_x, component)
-      table.insert(config.inactive_winbar.lualine_x, component)
-    end
 
     local function ins_left(component)
       table.insert(config.sections.lualine_c, component)
@@ -104,6 +122,14 @@ return {
 
     local function ins_right(component)
       table.insert(config.sections.lualine_x, component)
+    end
+
+    local function tab_left(component)
+      table.insert(config.tabline.lualine_c, component)
+    end
+
+    local function tab_right(component)
+      table.insert(config.tabline.lualine_x, component)
     end
 
     local theme = require("core.colors")
@@ -164,111 +190,125 @@ return {
       gui = "bold",
     }
 
-    --[[ WINBAR ]]
-    wins_left({
-      function()
-        return mode[vim.fn.mode()]
-      end,
-      -- "mode",
-      color = function()
-        return { fg = mode_color[vim.fn.mode()] }
-      end,
-      -- padding = { right = 1, left = 3 },
-    })
+    -- tabline
+    -- tabs
+    tab_left({
+      'tabs',
+      max_length = vim.o.columns / 3, -- Maximum width of tabs component.
+      -- Note:
+      -- It can also be a function that returns
+      -- the value of `max_length` dynamically.
+      mode = 0, -- 0: Shows tab_nr
+      -- 1: Shows tab_name
+      -- 2: Shows tab_nr + tab_name
 
-    wins_left({
-      "filename",
-      cond = conditions.buffer_not_empty,
-      color = { fg = colors.grey },
-      file_status = true,     -- Displays file status (readonly status, modified status)
-      newfile_status = false, -- Display new file status (new file means no write after created)
-      path = 1,               -- 0: Just the filename
-      -- 1: Relative path
-      -- 2: Absolute path
-      -- 3: Absolute path, with tilde as the home directory
+      -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+      use_mode_colors = false,
 
-      shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-      -- for other components. (terrible name, any suggestions?)
-      symbols = {
-        modified = "", -- Text to show when the file is modified.
-        readonly = "", -- Text to show when the file is non-modifiable or readonly.
-        unnamed = "ﲃ", -- Text to show for unnamed buffers.
-        newfile = "", -- Text to show for newly created file before first write
+      tabs_color = {
+        -- Same values as the general color option can be used here.
+        active = { fg = colors.purple, bg = colors.bg },    -- Color for active tab.
+        inactive = { fg = colors.bg2, bg = colors.bg },     -- Color for inactive tab.
       },
+
+      fmt = function(name, context)
+        -- Show + if buffer is modified in tab
+        local buflist = vim.fn.tabpagebuflist(context.tabnr)
+        local winnr = vim.fn.tabpagewinnr(context.tabnr)
+        local bufnr = buflist[winnr]
+        local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+        return name .. (mod == 1 and ' +' or '')
+      end
     })
 
-    wins_left({
-      "searchcount",
-      color = { fg = colors.green1 },
-    })
-
-    wins_left({
-      function()
-        return "%="
-      end,
-    })
-
-    wins_right({
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      symbols = { error = " ", warn = " ", info = " " },
-      diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
-      },
-      padding = { right = 1, left = 2 },
-    })
-
-    wins_right({
-      "diff",
-      symbols = { added = " ", modified = "柳", removed = " " },
-      diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-      },
-      cond = conditions.hide_in_width,
-      padding = { right = 2, left = 1 },
-    })
-
-    wins_right({
-      function()
-        return "  "
-      end,
-      color = { fg = theme.color20 },
-    })
-
-    -- Statusline
-    ins_left({
-      "filetype",
-      color = function()
-        return { fg = mode_color[vim.fn.mode()] }
-      end,
-      icon_only = false,
-      icon = { align = "left" },
-      padding = { right = 1, left = 3 },
-    })
-
-    ins_left({
-      function()
-        return '%{&ft == "toggleterm" ? " ".b:toggle_number."" : ""}'
-      end,
-      color = { fg = colors.green },
-    })
-
-    ins_left({
-      function()
-        return "%="
-      end,
-    })
-
-    ins_right({
+    -- datetime
+    tab_left({
       "datetime",
       color = "Keyword"
     })
 
-    ins_right({
+    -- sep
+    tab_left({
+      function()
+        return "%="
+      end,
+    })
+
+    -- buffers
+    tab_left({
+      'buffers',
+      show_filename_only = false,     -- Shows shortened relative path when set to false.
+      hide_filename_extension = true, -- Hide filename extension when set to true.
+      show_modified_status = true,    -- Shows indicator when the buffer is modified.
+      --
+      mode = 1,                       -- 0: Shows buffer name
+      --   -- 1: Shows buffer index
+      --   -- 2: Shows buffer name + buffer index
+      --   -- 3: Shows buffer number
+      --   -- 4: Shows buffer name + buffer number
+      --
+      --   max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+      --   -- it can also be a function that returns
+      --   -- the value of `max_length` dynamically.
+      --   filetype_names = {
+      --     TelescopePrompt = 'Telescope',
+      --     dashboard = 'Dashboard',
+      --     packer = 'Packer',
+      --     fzf = 'FZF',
+      --     alpha = 'Alpha'
+      --   }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+      --
+      --   -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+      --   use_mode_colors = false,
+      --
+      buffers_color = {
+        -- Same values as the general color option can be used here.
+        active = { fg = colors.purple, bg = colors.bg },    -- Color for active buffer.
+        inactive = { fg = colors.bg2, bg = colors.bg },     -- Color for inactive buffer.
+      },
+      --
+      symbols = {
+        modified = '',         -- Text to show when the buffer is modified
+        alternate_file = '',   -- Text to show to identify the alternate file
+        directory = '',        -- Text to show when the buffer is a directory
+      },
+    })
+
+    -- lazy updates
+    tab_right({
+      require("lazy.status").updates,
+      cond = require("lazy.status").has_updates,
+      color = { fg = "#ff9e64" },
+    })
+
+    -- wifi
+    tab_right({
+      "wifi",
+      status = {
+        connected = "󰤪",
+        disconnected = "󰤫",
+        show = true
+      },
+      essid = {
+        show = true
+      },
+      bit_rate = {
+        unit = "mbps",
+        show = true
+      }
+    })
+
+    -- ram
+    tab_right({
+      "ram",
+      icon = "󰍛",
+      show_percentage = false,
+      color = { fg = theme.color26 },
+    })
+
+    -- battery
+    tab_right({
       "battery",
       show_status_text = false,
       view = {
@@ -299,30 +339,75 @@ return {
       color = { fg = colors.orange3 },
     })
 
-    ins_right({
-      "wifi",
-      status = {
-        connected = "󰤪",
-        disconnected = "󰤫",
-        show = true
-      },
-      essid = {
-        show = true
-      },
-      bit_rate = {
-        unit = "mbps",
-        show = true
-      }
+    -- Statusline
+    -- mode
+    ins_left({
+      -- "mode",
+      function()
+        return mode[vim.fn.mode()]
+      end,
+      -- "mode",
+      color = function()
+        return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
+      end,
+      -- padding = { right = 1, left = 3 },
     })
 
-    ins_right({
-      "ram",
-      icon = "󰍛",
-      show_percentage = false,
-      color = { fg = theme.color26 },
+    -- filetype
+    ins_left({
+      "filetype",
+      icon_only = false,
+      icon = { align = "left" },
+      color = function()
+        return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
+      end,
+      -- padding = { right = 1, left = 3 },
     })
 
+    -- filename
+    ins_left({
+      "filename",
+      cond = conditions.buffer_not_empty,
+      color = { fg = colors.grey },
+      file_status = true,     -- Displays file status (readonly status, modified status)
+      newfile_status = false, -- Display new file status (new file means no write after created)
+      path = 1,               -- 0: Just the filename
+      -- 1: Relative path
+      -- 2: Absolute path
+      -- 3: Absolute path, with tilde as the home directory
 
+      shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+      -- for other components. (terrible name, any suggestions?)
+      symbols = {
+        modified = "", -- Text to show when the file is modified.
+        readonly = "", -- Text to show when the file is non-modifiable or readonly.
+        unnamed = "ﲃ", -- Text to show for unnamed buffers.
+        newfile = "", -- Text to show for newly created file before first write
+      },
+    })
+
+    -- search count
+    ins_left({
+      "searchcount",
+      color = { fg = colors.green1 },
+    })
+
+    -- toggleterm
+    ins_left({
+      function()
+        return '%{&ft == "toggleterm" ? " ".b:toggle_number."" : ""}'
+      end,
+      color = { fg = colors.green },
+    })
+
+    -- sep
+    ins_left({
+      function()
+        return "%="
+      end,
+    })
+
+    -- macros etc
     if on then
       ins_left({
         noice.api.statusline.mode.get,
@@ -332,10 +417,30 @@ return {
       })
     end
 
+    -- diagnosticcs
     ins_right({
-      require("lazy.status").updates,
-      cond = require("lazy.status").has_updates,
-      color = { fg = "#ff9e64" },
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      symbols = { error = " ", warn = " ", info = " " },
+      diagnostics_color = {
+        color_error = { fg = colors.red },
+        color_warn = { fg = colors.yellow },
+        color_info = { fg = colors.cyan },
+      },
+      padding = { right = 1, left = 1 },
+    })
+
+    -- git diffs
+    ins_right({
+      "diff",
+      symbols = { added = " ", modified = "柳", removed = " " },
+      diff_color = {
+        added = { fg = colors.green },
+        modified = { fg = colors.orange },
+        removed = { fg = colors.red },
+      },
+      cond = conditions.hide_in_width,
+      padding = { right = 1, left = 1 },
     })
 
     -- ins_right({
@@ -346,10 +451,18 @@ return {
     -- })
 
     ins_right({
+      function()
+        return ""
+      end,
+      color = { bg = colors.yellow, fg = colors.bg },
+    })
+
+    -- branch
+    ins_right({
       "branch",
-      icon = { "", align = "left" },
-      color = { fg = colors.yellow },
-      padding = { right = 1, left = 1 },
+      -- icon = { "", align = "left" },
+      color = { bg = colors.yellow, fg = colors.bg },
+      padding = { right = 1, left = 0 },
     })
 
     lualine.setup(config)
