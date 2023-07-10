a2e03dcf18ff1bb4fcf15c567ced5d56a8efdf45
Pheon-Dev <devpheon@gmail.com>
2023-07-10 22:16:44 +0300

    [-] keys

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,40 +1,7 @@
 return {
   {
     "Exafunction/codeium.vim",
-    keys = {
-      {
-        "<C-l>",
-        mode = { "i" },
-        function()
-  return vim.fn["codeium#Accept"]()
-        end,
-        desc = "Accept Suggestion",
-      },
-      {
-        "<C-n>",
-        mode = { "i" },
-        function()
-  return vim.fn["codeium#CycleCompletions"](1)
-        end,
-        desc = "Cycle Completions Forward",
-      },
-      {
-        "<C-p>",
-        mode = { "i" },
-        function()
-  return vim.fn["codeium#CycleCompletions"](-1)
-        end,
-        desc = "Cycle Completions Backward",
-      },
-      {
-        "<C-u>",
-        mode = { "i" },
-        function()
-  return vim.fn["codeium#Clear"]()
-        end,
-        desc = "Clear Completions",
-      },
-    },
+    event = "BufReadPre",
     config = function()
     end,
   },
