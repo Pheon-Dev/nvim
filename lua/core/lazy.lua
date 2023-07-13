63d7e254f5ccb23071e04246cf1ff2b4ab3bcedb
Pheon-Dev <devpheon@gmail.com>
2023-07-13 19:35:06 +0300

    [-] changes made notification

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -112,7 +112,7 @@ require("lazy").setup({
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = true, -- get a notification when changes are found
+    notify = false, -- get a notification when changes are found
   },
   performance = {
     cache = {
