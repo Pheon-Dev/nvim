36ffd8a953bed95d2ab6157547042349a459e4e8
Pheon-Dev <devpheon@gmail.com>
2023-07-16 17:43:08 +0300

    [-] neodev [+] multi select [+] virtcolumn

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -56,6 +56,16 @@ return {
     enabled = false,
   },
   {
+    "xiyaowong/virtcolumn.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    enabled = true,
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = { "BufReadPost", "BufNewFile" },
+    enabled = true,
+  },
+  {
     "norcalli/nvim-colorizer.lua",
     event = { "BufReadPost", "BufNewFile" },
     enabled = true,
@@ -141,32 +151,33 @@ return {
   },
 
   -- better vim.ui
-  -- {
-  -- "stevearc/dressing.nvim",
-  -- init = function()
-  --   ---@diagnostic disable-next-line: duplicate-set-field
-  --   vim.ui.select = function(...)
-  --     require("lazy").load({ plugins = { "dressing.nvim" } })
-  --     return vim.ui.select(...)
-  --   end
-  --   ---@diagnostic disable-next-line: duplicate-set-field
-  --   vim.ui.input = function(...)
-  --     require("lazy").load({ plugins = { "dressing.nvim" } })
-  --     return vim.ui.input(...)
-  --   end
-  -- end,
-  -- config = function()
-  --   require("dressing").setup {
-  --     input = {
-  --       override = function(conf)
-  --         conf.col = -1
-  --         conf.row = 0
-  --         return conf
-  --       end,
-  --     },
-  --   }
-  -- end
-  -- },
+
+  {
+  "stevearc/dressing.nvim",
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.input(...)
+    end
+  end,
+  config = function()
+    require("dressing").setup {
+      input = {
+        override = function(conf)
+          conf.col = -1
+          conf.row = 0
+          return conf
+        end,
+      },
+    }
+  end
+  },
 
   -- icons
   "nvim-tree/nvim-web-devicons",
