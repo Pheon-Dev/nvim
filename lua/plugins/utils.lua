b956ddd33b39c3d60b4d2bced5493f2e673b5f43
Pheon-Dev <devpheon@gmail.com>
2023-07-21 10:55:46 +0300

    [+] updates

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -153,30 +153,30 @@ return {
   -- better vim.ui
 
   {
-  "stevearc/dressing.nvim",
-  init = function()
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.select = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.select(...)
+    "stevearc/dressing.nvim",
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+    config = function()
+      require("dressing").setup {
+        input = {
+          override = function(conf)
+            conf.col = -1
+            conf.row = 0
+            return conf
+          end,
+        },
+      }
     end
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.input = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.input(...)
-    end
-  end,
-  config = function()
-    require("dressing").setup {
-      input = {
-        override = function(conf)
-          conf.col = -1
-          conf.row = 0
-          return conf
-        end,
-      },
-    }
-  end
   },
 
   -- icons
