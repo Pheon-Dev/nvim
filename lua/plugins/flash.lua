1445f763fac1b47a2f3bf5c10c8630b7ce376b23
Pheon-Dev <devpheon@gmail.com>
2023-07-22 13:35:46 +0300

    [~] update flash config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -9,7 +9,9 @@ return {
         "s",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump()
+          require("flash").jump({
+            --
+          })
         end,
         desc = "Flash",
       },
@@ -69,7 +71,7 @@ return {
           --   end,
           mode = "exact",
           -- behave like `incsearch`
-          incremental = true,
+          incremental = false,
           -- Excluded filetypes and custom window filters
           ---@type (string|fun(win:window))[]
           exclude = {
@@ -117,6 +119,7 @@ return {
         label = {
           -- allow uppercase labels
           uppercase = true,
+          -- exclude = "",
           -- add a label for the first match in the current window.
           -- you can always jump to the first match with `<CR>`
           current = true,
@@ -186,38 +189,55 @@ return {
             highlight = { backdrop = false },
             jump = { history = true, register = true, nohlsearch = true },
             search = {
-              -- `forward` will be automatically set to the search direction
-              -- `mode` is always set to `search`
-              -- `incremental` is set to `true` when `incsearch` is enabled
+              -- forward = "", -- `forward` will be automatically set to the search direction
+              -- mode = "search",     -- `mode` is always set to `search`
+              -- incremental = false, -- `incremental` is set to `true` when `incsearch` is enabled
             },
           },
           -- options used when flash is activated through
           -- `f`, `F`, `t`, `T`, `;` and `,` motions
           char = {
-            enabled = false,
-            -- when to hide flash
-            autohide = function(motion)
-              -- autohide flash when the operator is `y`
-              return vim.fn.mode(true):find("no") and vim.v.operator == "y"
+            enabled = true,
+            -- dynamic configuration for ftFT motions
+            config = function(opts)
+              -- autohide flash when in operator-pending mode
+              opts.autohide = vim.fn.mode(true):find("no") and vim.v.operator == "y"
+
+              -- disable jump labels when enabled and when using a count
+              opts.jump_labels = opts.jump_labels and vim.v.count == 0
+
+              -- Show jump labels only in operator-pending mode
+              -- opts.jump_labels = vim.v.count == 0 and vim.fn.mode(true):find("o")
             end,
-            -- when to show jump labels
-            jump_labels = function(motion)
-              -- never show jump labels by default
-              return true
-              -- Always show jump labels for ftFT
-              -- return vim.v.count == 0 and motion:find("[ftFT]")
-              -- Show jump labels for ftFT in operator-pending mode
-              -- return vim.v.count == 0 and motion:find("[ftFT]") and vim.fn.mode(true):find("o")
-            end,
+            -- hide after jump when not using jump labels
+            autohide = false,
+            -- show jump labels
+            jump_labels = true,
+            -- set to `false` to use the current line only
+            multi_line = true,
             -- When using jump labels, don't use these keys
             -- This allows using those keys directly after the motion
-            -- label = { exclude = "hjkliardc" },
+            label = { exclude = "hjkliardc" },
             -- by default all keymaps are enabled, but you can disable some of them,
             -- by removing them from the list.
             -- If you rather use another key, you can map them
             -- to something else, e.g., { [";"] = "L", [","] = H }
-            -- keys = { "f", "F", "t", "T" },
-            keys = { "f", "F" },
+            keys = { "f", "F", "t", "T", ";", "," },
+            ---@alias Flash.CharActions table<string, "next" | "prev" | "right" | "left">
+            -- The direction for `prev` and `next` is determined by the motion.
+            -- `left` and `right` are always left and right.
+            char_actions = function(motion)
+              return {
+                [";"] = "next", -- set to `right` to always go right
+                [","] = "prev", -- set to `left` to always go left
+                -- clever-f style
+                [motion:lower()] = "next",
+                [motion:upper()] = "prev",
+                -- jump2d style: same case goes next, opposite case goes prev
+                -- [motion] = "next",
+                -- [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
+              }
+            end,
             search = { wrap = false },
             highlight = { backdrop = true },
             jump = { register = false },
@@ -225,13 +245,20 @@ return {
           -- options used for treesitter selections
           -- `require("flash").treesitter()`
           treesitter = {
-            labels = "asdfghjklrewquiopytbnvmcxz",
+            labels = "abcdefghijklmnopqrstuvwxyz",
             jump = { pos = "range" },
+            search = { incremental = false },
+            label = { before = true, after = true, style = "inline" },
             highlight = {
-              label = { before = true, after = true, style = "inline" },
               backdrop = false,
               matches = false,
             },
+          },
+          treesitter_search = {
+            jump = { pos = "range" },
+            search = { multi_window = true, wrap = true, incremental = false },
+            remote_op = { restore = true },
+            label = { before = true, after = true, style = "inline" },
           },
           -- options used for remote flash
           remote = {
@@ -264,19 +291,19 @@ return {
           motion = false,
         },
       })
-      local Config = require("flash.config")
-      local Char = require("flash.plugins.char")
-      for _, motion in ipairs({ "f", "F", "t", "T" }) do
-        vim.keymap.set({ "n", "x", "o" }, motion, function()
-          require("flash").jump(Config.get({
-            mode = "char",
-            search = {
-              mode = Char.mode(motion),
-              max_length = 1,
-            },
-          }, Char.motions[motion]))
-        end)
-      end
+      -- local Config = require("flash.config")
+      -- local Char = require("flash.plugins.char")
+      -- for _, motion in ipairs({ "f", "F", "t", "T" }) do
+      --   vim.keymap.set({ "n", "x", "o" }, motion, function()
+      --     require("flash").jump(Config.get({
+      --       mode = "char",
+      --       search = {
+      --         mode = Char.mode(motion),
+      --         max_length = 1,
+      --       },
+      --     }, Char.motions[motion]))
+      --   end)
+      -- end
     end,
   },
   {
