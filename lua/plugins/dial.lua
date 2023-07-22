4f1c09fdf78152bf14acd95cad2d382b1df36948
Pheon-Dev <devpheon@gmail.com>
2023-07-22 11:28:39 +0300

    [-] dial keys

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -3,8 +3,8 @@ return {
   -- stylua: ignore
   event = { "BufReadPost", "BufNewFile" },
   keys = {
-    { "+", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-    { "_", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+    { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
   },
   config = function()
     local augend = require("dial.augend")
