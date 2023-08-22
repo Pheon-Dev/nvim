f2989e9249ba9d70d99f92732ecb855a2620ae21
Pheon-Dev <devpheon@gmail.com>
2023-08-22 12:52:10 +0300

    [~] laxy loading

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -8,9 +8,9 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("go").setup({ lsp_inlay_hints = { enable = false } })
+      require("go").setup({ lsp_inlay_hints = { enable = true } })
     end,
-    event = { "CmdlineEnter" },
+    -- event = { "CmdlineEnter" },
     ft = { "go", 'gomod' },
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
