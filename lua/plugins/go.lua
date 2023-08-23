c2ff7aa1c872677b83602c24f38243597b9af803
Pheon-Dev <devpheon@gmail.com>
2023-08-23 15:02:43 +0300

    [~] go.nvim events

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -11,8 +11,8 @@ return {
       require("go").setup({ lsp_inlay_hints = { enable = true } })
     end,
     -- event = { "CmdlineEnter" },
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    -- ft = { "go", 'gomod' },
+    -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
   -- { "nsf/gocode" },
   -- {
