e80af8517c10fc2502ab30211c91b054043f9e60
Pheon-Dev <devpheon@gmail.com>
2023-08-25 13:58:03 +0300

    [-] more unused plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -10,16 +10,28 @@ return {
     "chaoren/vim-wordmotion",
     event = { "BufReadPost", "BufNewFile" },
   },
-  -- {
-  --   'kevinhwang91/nvim-fundo',
-  --   dependencies = 'kevinhwang91/promise-async',
-  --   run = function()
-  --     require('fundo').install()
-  --   end,
-  --   config = function()
-  --     require('fundo').setup()
-  --   end
-  -- },
+  {
+    "monaqa/dial.nvim",
+    -- stylua: ignore
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      -- { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      -- { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    },
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias["%Y/%m/%d"],
+          augend.constant.alias.bool,
+          augend.semver.alias.semver,
+          augend.constant.new({ elements = { "let", "const" } }),
+        },
+      })
+    end,
+  },
   {
     "voldikss/vim-floaterm",
     event = "VeryLazy",
