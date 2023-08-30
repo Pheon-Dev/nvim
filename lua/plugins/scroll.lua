c41302628dcc25fa4a7f9b71ea7cf925098d6a35
Pheon-Dev <devpheon@gmail.com>
2023-08-30 19:12:35 +0300

    [~] remapped scroll keys

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -24,7 +24,6 @@ return {
         scroll_limit = 150,       -- Max number of lines moved before scrolling is skipped. Setting
         -- to -1 will disable this option.
       }
-
       -- Half-window movements:
       vim.keymap.set({ 'n', 'x' }, '<C-u>', "<Cmd>lua Scroll('<C-u>', 1, 1)<CR>")
       vim.keymap.set({ 'n', 'x' }, '<C-d>', "<Cmd>lua Scroll('<C-d>', 1, 1)<CR>")
@@ -44,7 +43,9 @@ return {
       -- Start/end of line:
       vim.keymap.set({ 'n', 'x' }, '0', "<Cmd>lua Scroll('0')<CR>")
       vim.keymap.set({ 'n', 'x' }, '^', "<Cmd>lua Scroll('^')<CR>")
+      vim.keymap.set({ 'n', 'x' }, 'H', "<Cmd>lua Scroll('^')<CR>")
       vim.keymap.set({ 'n', 'x' }, '$', "<Cmd>lua Scroll('$', 0, 1)<CR>")
+      vim.keymap.set({ 'n', 'x' }, 'L', "<Cmd>lua Scroll('$', 0, 1)<CR>")
 
       -- Paragraph movements:
       vim.keymap.set({ 'n', 'x' }, '{', "<Cmd>lua Scroll('{')<CR>")
@@ -61,6 +62,8 @@ return {
       -- Previous/next cursor location:
       vim.keymap.set('n', '<C-o>', "<Cmd>lua Scroll('<C-o>', 1)<CR>")
       vim.keymap.set('n', '<C-i>', "<Cmd>lua Scroll('1<C-i>', 1)<CR>")
+      vim.keymap.set('n', 'go', "<Cmd>lua Scroll('<C-o>', 1)<CR>")
+      vim.keymap.set('n', 'gi', "<Cmd>lua Scroll('1<C-i>', 1)<CR>")
 
       -- Screen scrolling:
       vim.keymap.set('n', 'zz', "<Cmd>lua Scroll('zz', 0, 1)<CR>")
@@ -111,57 +114,6 @@ return {
       -- LSP go-to-declaration:
       vim.keymap.set('n', 'gD', "<Cmd>lua Scroll('declaration')<CR>")
     end
-  },
-  {
-    "karb94/neoscroll.nvim",
-    enabled = false,
-    event = { "BufReadPost", "BufNewFile" },
-    -- event = "VeryLazy",
-    config = function()
-      local ok, neoscroll = pcall(require, "neoscroll")
-
-      if not ok then
-        return
-      end
-      -- local ot, ts_ctx = require("treesitter-context")
-
-      neoscroll.setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = "cubic",   -- quadratic, cubic, quartic, quintic, circular, sine
-        pre_hook = function()        -- Function to run before the scrolling animation starts
-          -- ts_ctx.disable()
-          -- vim.cmd [[TSBufToggle highlight]]
-        end,
-        post_hook = function() -- Function to run after the scrolling animation ends
-          -- ts_ctx.enable()
-          -- vim.cmd [[TSBufToggle highlight]]
-        end,
-        performance_mode = false, -- Disable "Performance Mode" on all buffers.
-      })
-
-      -- Syntax scrolling function: `scroll(lines, move_cursor, time[, easing_function_name])`
-      local t = {}
-      -- Syntax: t[keys] = {function, {function arguments}}
-      t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '250' } }
-      t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '250' } }
-      t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '450' } }
-      t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450' } }
-      t['<C-y>'] = { 'scroll', { '-0.10', 'false', '100' } }
-      t['<C-e>'] = { 'scroll', { '0.10', 'false', '100' } }
-      t['<C-h>'] = { 'zt', { '250' } }
-      t['<C-l>'] = { 'zb', { '250' } }
-      t['M'] = { 'zz', { '250' } }
-      t['zt'] = { 'zt', { '250' } }
-      t['zz'] = { 'zz', { '250' } }
-      t['zb'] = { 'zb', { '250' } }
-
-      require("neoscroll.config").set_mappings(t)
-    end,
   },
 }
 
