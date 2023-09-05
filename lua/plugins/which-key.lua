cc0c9e0e6f1e13be86bc7d6cbf8472ec494d8750
Pheon-Dev <devpheon@gmail.com>
2023-09-05 09:15:41 +0300

    [~] messages using Noice

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -124,7 +124,7 @@ return {
       k = { ":lua require('harpoon.ui').toggle_quick_menu()<cr>", "Harpoon" },
       m = { ":Antelope marks<cr>", "Marks" },
       -- n = { ":Telescope notify initial_mode=normal<cr>", "Notifications" },
-      n = { ":messages<cr>", "Messages" },
+      n = { ":Noice<cr>", "Messages" },
       o = {
         name = "Others",
         m = { ":Mason<cr>", "Mason" },
