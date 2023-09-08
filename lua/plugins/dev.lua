49009c9d12c71900fc27b168da1f56c69d281d1f
Pheon-Dev <devpheon@gmail.com>
2023-09-08 12:19:16 +0300

    [+] neodev

---
+++ lua/plugins/dev.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/neodev.nvim",
+  enabled = true,
+  -- event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    debug = true,
+    experimental = {
+      pathStrict = true,
+    },
+  },
+}
+
