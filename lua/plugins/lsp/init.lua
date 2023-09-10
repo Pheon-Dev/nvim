128d42d8e378dbfdd3454bce8cf12f613c6209ec
Pheon-Dev <devpheon@gmail.com>
2023-09-10 19:34:54 +0300

    [+] lspsaga

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -55,4 +55,16 @@ return {
       })
     end,
   },
+  {
+    'nvimdev/lspsaga.nvim',
+    enabled = false,
+    config = function()
+      require('lspsaga').setup({})
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    }
+  },
+
 }
