54db8a4dc4ea53812b69d2c25d25e031d48087cc
Pheon-Dev <devpheon@gmail.com>
2023-10-31 11:58:35 +0300

    [~] using number for mru

--- lua/config/utils/dashboard.lua
+++ lua/config/utils/dashboard.lua
@@ -9,7 +9,7 @@ M.config = function()
   dashboard.setup({
     -- theme = 'hyper',             --  theme is doom and hyper default is hyper
     disable_move = false,       --  default is false disable move keymap for hyper
-    shortcut_type = "letter",   --  shorcut type 'letter' or 'number'
+    shortcut_type = "number",   --  shorcut type 'letter' or 'number'
     change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
     config = {
       header = M.header,
