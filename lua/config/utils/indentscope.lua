a81d7ca3dbf5f68302077afbcbb4cbab3c84b0a9
Pheon-Dev <devpheon@gmail.com>
2023-11-15 09:26:40 +0300

    [+] oil to indentscope

--- lua/config/utils/indentscope.lua
+++ lua/config/utils/indentscope.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.config = function()
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "alpha", "lazy", "NvimTree", "Mason", "floaterm" },
+    pattern = { "alpha", "lazy", "NvimTree", "Mason", "floaterm", "oil" },
     callback = function()
       vim.b.miniindentscope_disable = true
     end,
