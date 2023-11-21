9af12e239b4b677c1862a79b4481c15c20dc4f70
Pheon-Dev <devpheon@gmail.com>
2023-11-21 16:48:37 +0300

    [-] noh

--- lua/config/eyecandy/hlslens.lua
+++ lua/config/eyecandy/hlslens.lua
@@ -16,7 +16,7 @@ M.config = function()
   vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
   vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
-  vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
+  -- vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
 end
 
 return M
