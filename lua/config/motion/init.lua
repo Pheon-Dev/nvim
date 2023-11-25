1ed60619b6ded11c2cfa441528960659f22c4e3c
Pheon-Dev <devpheon@gmail.com>
2023-11-25 15:28:05 +0300

    [+] motion module

---
+++ lua/config/motion/init.lua
@@ -0,0 +1,7 @@
+local config = require("config.motion")
+
+local M = {}
+
+M.flash = config.flash
+
+return M
--- lua/config/navigation/init.lua
+++ lua/config/navigation/init.lua
@@ -4,7 +4,6 @@ local M = {}
 
 M.antelope = config.antelope
 M.buffalo = config.buffalo
-M.flash = config.flash
 M.oil = config.oil
 M.harpoon = config.harpoon
 M.nvim_tree = config.nvim_tree
