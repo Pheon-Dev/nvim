51219957c4bf757a9b07556a13c5b0ee37320453
Pheon-Dev <devpheon@gmail.com>
2024-01-10 09:10:06 +0300

    [~] spiders text objects

--- lua/config/motion/spider.lua
+++ lua/config/motion/spider.lua
@@ -6,6 +6,17 @@ M.config = function()
     subwordMovement = true,
     customPatterns = {}, -- check Custom Movement Patterns for details
   }
+  vim.keymap.set("i", "<C-f>", "<Esc>l<cmd>lua require('spider').motion('w')<CR>i")
+  vim.keymap.set("i", "<C-b>", "<Esc><cmd>lua require('spider').motion('b')<CR>i")
+  vim.keymap.set("i", "<C-u>", "<Esc>k<CR>i")
+  vim.keymap.set("i", "<C-d>", "<Esc>j<CR>i")
+
+  vim.keymap.set("o", "w", "<cmd>lua require('spider').motion('w')<CR>")
+  -- vim.keymap.set("n", "cw", "ce", { remap = true })
+
+  -- or the same with as one mapping without `remap = true`
+  -- vim.keymap.set("n", "cw", "c<cmd>lua require('spider').motion('e')<CR>")
+
   vim.keymap.set(
     { "n", "o", "x" },
     "w",
