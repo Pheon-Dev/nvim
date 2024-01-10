a9d8ce27f462cfcd339053488457d7a4b70e72c7
Pheon-Dev <devpheon@gmail.com>
2024-01-10 11:13:25 +0300

    [~] eyeliner highlight always

--- lua/config/motion/eyeliner.lua
+++ lua/config/motion/eyeliner.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.config = function()
   require('eyeliner').setup {
-    highlight_on_key = true, -- show highlights only after keypress
-    dim = true,              -- dim all other characters if set to true (recommended!)
+    highlight_on_key = false, -- show highlights only after keypress
+    dim = true,               -- dim all other characters if set to true (recommended!)
   }
 end
 
