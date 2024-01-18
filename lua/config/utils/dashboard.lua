db9017c391c04ea6db0b569623d72e2e5818f15b
Pheon-Dev <devpheon@gmail.com>
2024-01-18 08:15:17 +0300

    [+] formatting

--- lua/config/utils/dashboard.lua
+++ lua/config/utils/dashboard.lua
@@ -6,11 +6,13 @@ M.dependencies = {}
 M.config = function()
   local has_dashboard, dashboard = pcall(require, "dashboard")
 
-  if not has_dashboard then return end
+  if not has_dashboard then
+    return
+  end
   dashboard.setup({
-    theme = 'hyper',            --  theme is doom and hyper default is hyper
-    disable_move = false,       --  default is false disable move keymap for hyper
-    shortcut_type = "number",   --  shorcut type 'letter' or 'number'
+    theme = "hyper", --  theme is doom and hyper default is hyper
+    disable_move = false, --  default is false disable move keymap for hyper
+    shortcut_type = "number", --  shorcut type 'letter' or 'number'
     change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
     config = {
       header = M.header,
@@ -18,11 +20,11 @@ M.config = function()
       footer = M.footer,
       project = M.project,
       mru = M.mru,
-    },                   --  config used for theme
+    }, --  config used for theme
     hide = {
       statusline = true, -- hide statusline default is true
-      tabline = true,    -- hide the tabline
-      winbar = true,     -- hide winbar
+      tabline = true, -- hide the tabline
+      winbar = true, -- hide winbar
     },
     -- preview = {
     --   command     -- preview command
@@ -58,21 +60,16 @@ M.confirm_key = "l"
 
 M.header = {
   [[                                                                      ]],
-  [[______________________________________________________________________]],
-  [[                                                                      ]],
-  [[                                                                      ]],
-  [[                                                                      ]],
-  [[                                                                      ]],
 }
 
 M.shortcut = {
   {
-    icon = ' ',
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = '@property',
-    action = 'FloatermNew lf',
-    key = 'j'
+    icon = " ",
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "@property",
+    action = "FloatermNew lf",
+    key = "j",
   },
   --[[ {
     icon = ' ',
@@ -91,67 +88,67 @@ M.shortcut = {
     key = 'k'
   }, ]]
   {
-    icon = ' ',
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = 'DiagnosticInfo',
-    action = 'FloatermNew lazygit',
-    key = 'l',
+    icon = " ",
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "DiagnosticInfo",
+    action = "FloatermNew lazygit",
+    key = "l",
   },
   {
-    icon = '󰊳 ',
-    desc = '',
-    group = 'DashboardIcon',
-    icon_hl = '@property',
-    action = 'Lazy update',
-    key = 'u'
+    icon = "󰊳 ",
+    desc = "",
+    group = "DashboardIcon",
+    icon_hl = "@property",
+    action = "Lazy update",
+    key = "u",
   },
   {
-    icon = '󰱽 ',
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = 'Label',
-    action = 'FloatermNew fzf',
-    key = 'f',
+    icon = "󰱽 ",
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "Label",
+    action = "FloatermNew fzf",
+    key = "f",
   },
   {
-    icon = '󰺯 ',
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = 'DiagnosticHint',
-    action = 'FloatermNew rg',
-    key = 's',
+    icon = "󰺯 ",
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "DiagnosticHint",
+    action = "FloatermNew rg",
+    key = "s",
   },
   {
-    icon = ' ',
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = '@property',
-    action = 'FloatermNew',
-    key = '`',
+    icon = " ",
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "@property",
+    action = "FloatermNew",
+    key = "`",
   },
   {
     icon = "鈴",
-    group = 'DashboardIcon',
-    desc = '',
-    icon_hl = 'Number',
-    action = 'Lazy',
-    key = 'z'
+    group = "DashboardIcon",
+    desc = "",
+    icon_hl = "Number",
+    action = "Lazy",
+    key = "z",
   },
   {
-    icon = '󰟿 ',
-    desc = '',
-    group = 'DiagnosticError',
-    icon_hl = 'DiagnosticError',
-    action = 'q',
-    key = 'h',
+    icon = "󰟿 ",
+    desc = "",
+    group = "DiagnosticError",
+    icon_hl = "DiagnosticError",
+    action = "q",
+    key = "h",
   },
 }
 
 M.footer = {}
 
-M.project = { enable = false, limit = 8, icon = 'your icon', label = '', action = 'FloatermNew fzf' }
+M.project = { enable = false, limit = 8, icon = "your icon", label = "", action = "FloatermNew fzf" }
 
-M.mru = { limit = 8, icon = '↘', label = '', }
+M.mru = { limit = 8, icon = "↘", label = "" }
 
 return M
