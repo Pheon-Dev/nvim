25363c7bda531493b5ad45b3219b3c7a1578d63f
Pheon-Dev <devpheon@gmail.com>
2024-02-16 11:22:11 +0300

    [+] oil vcs

--- lua/config/navigation/oil.lua
+++ lua/config/navigation/oil.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.dependencies = { "nvim-tree/nvim-web-devicons" }
+M.dependencies = { "nvim-tree/nvim-web-devicons", "SirZenith/oil-vcs-status" }
 
 M.config = function()
   require("oil").setup({
@@ -23,7 +23,7 @@ M.config = function()
     -- Window-local options to use for oil buffers
     win_options = {
       wrap = false,
-      signcolumn = "no",
+      signcolumn = "yes:2",
       cursorcolumn = false,
       foldcolumn = "0",
       spell = false,
@@ -140,6 +140,117 @@ M.config = function()
       win_options = {
         winblend = 0,
       },
+    },
+  })
+
+  local status_const = require("oil-vcs-status.constant.status")
+
+  local StatusType = status_const.StatusType
+
+  require("oil-vcs-status").setup({
+    -- Executable path of each version control system.
+    vcs_executable = {
+      git = "git",
+      svn = "svn",
+    },
+
+    -- If trigger time between two file system event of the same entry is less
+    -- than this value, newer event will be ignored, unit: milisecond.
+    ---@type integer
+    fs_event_debounce = 500,
+
+    -- Sign character used by each status.
+    ---@type table<oil-vcs-status.StatusType, string>
+    status_symbol = {
+      [StatusType.Added] = "",
+      [StatusType.Copied] = "󱂬",
+      [StatusType.Deleted] = "",
+      [StatusType.Ignored] = "!",
+      [StatusType.Modified] = "",
+      [StatusType.Renamed] = "",
+      [StatusType.TypeChanged] = "T",
+      [StatusType.Unmodified] = " ",
+      [StatusType.Unmerged] = "U",
+      [StatusType.Untracked] = "?",
+      [StatusType.External] = "X",
+
+      [StatusType.UpstreamAdded] = "A",
+      [StatusType.UpstreamCopied] = "C",
+      [StatusType.UpstreamDeleted] = "D",
+      [StatusType.UpstreamIgnored] = "!",
+      [StatusType.UpstreamModified] = "M",
+      [StatusType.UpstreamRenamed] = "R",
+      [StatusType.UpstreamTypeChanged] = "T",
+      [StatusType.UpstreamUnmodified] = " ",
+      [StatusType.UpstreamUnmerged] = "U",
+      [StatusType.UpstreamUntracked] = "?",
+      [StatusType.UpstreamExternal] = "X",
+    },
+
+    -- Highlight group name used by each status type.
+    ---@type table<oil-vcs-status.StatusType, string | false>
+    status_hl_group = {
+      [StatusType.Added] = "OilVcsStatusAdded",
+      [StatusType.Copied] = "OilVcsStatusCopied",
+      [StatusType.Deleted] = "OilVcsStatusDeleted",
+      [StatusType.Ignored] = "OilVcsStatusIgnored",
+      [StatusType.Modified] = "OilVcsStatusModified",
+      [StatusType.Renamed] = "OilVcsStatusRenamed",
+      [StatusType.TypeChanged] = "OilVcsStatusTypeChanged",
+      [StatusType.Unmodified] = "OilVcsStatusUnmodified",
+      [StatusType.Unmerged] = "OilVcsStatusUnmerged",
+      [StatusType.Untracked] = "OilVcsStatusUntracked",
+      [StatusType.External] = "OilVcsStatusExternal",
+
+      [StatusType.UpstreamAdded] = "OilVcsStatusUpstreamAdded",
+      [StatusType.UpstreamCopied] = "OilVcsStatusUpstreamCopied",
+      [StatusType.UpstreamDeleted] = "OilVcsStatusUpstreamDeleted",
+      [StatusType.UpstreamIgnored] = "OilVcsStatusUpstreamIgnored",
+      [StatusType.UpstreamModified] = "OilVcsStatusUpstreamModified",
+      [StatusType.UpstreamRenamed] = "OilVcsStatusUpstreamRenamed",
+      [StatusType.UpstreamTypeChanged] = "OilVcsStatusUpstreamTypeChanged",
+      [StatusType.UpstreamUnmodified] = "OilVcsStatusUpstreamUnmodified",
+      [StatusType.UpstreamUnmerged] = "OilVcsStatusUpstreamUnmerged",
+      [StatusType.UpstreamUntracked] = "OilVcsStatusUpstreamUntracked",
+      [StatusType.UpstreamExternal] = "OilVcsStatusUpstreamExternal",
+    },
+
+    -- Sign priority of each staus. When sign column width is less then staus
+    -- symbol number, symbol with higher priority will be shown.
+    -- If signcolumn is wide enough, signs will be display from left to right in
+    -- order of priority from low to high.
+    ---@type table<oil-vcs-status.StatusType, number>
+    status_priority = {
+      [StatusType.UpstreamIgnored] = 0,
+      [StatusType.Ignored] = 0,
+
+      [StatusType.UpstreamUntracked] = 1,
+      [StatusType.Untracked] = 1,
+
+      [StatusType.UpstreamUnmodified] = 2,
+      [StatusType.Unmodified] = 2,
+      [StatusType.UpstreamExternal] = 2,
+      [StatusType.External] = 2,
+
+      [StatusType.UpstreamCopied] = 3,
+      [StatusType.UpstreamRenamed] = 3,
+      [StatusType.UpstreamTypeChanged] = 3,
+
+      [StatusType.UpstreamDeleted] = 4,
+      [StatusType.UpstreamModified] = 4,
+      [StatusType.UpstreamAdded] = 4,
+
+      [StatusType.UpstreamUnmerged] = 5,
+
+      [StatusType.Copied] = 13,
+      [StatusType.Renamed] = 13,
+      [StatusType.TypeChanged] = 13,
+
+      [StatusType.Deleted] = 14,
+      [StatusType.Modified] = 14,
+      [StatusType.Added] = 14,
+
+      [StatusType.Unmerged] = 15,
     },
   })
 end
