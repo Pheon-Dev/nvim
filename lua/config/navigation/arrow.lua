900fb769c2900342a21589a26ee63da2893f0902
Pheon-Dev <devpheon@gmail.com>
2024-03-22 08:31:43 +0300

    [+] arrow next and previous keymaps

--- lua/config/navigation/arrow.lua
+++ lua/config/navigation/arrow.lua
@@ -6,6 +6,9 @@ M.config = function()
     show_icons = true,
     leader_key = "'", -- Recommended to be a single key
   })
+  vim.keymap.set("n", "<C-j>", require("arrow.persist").previous)
+  vim.keymap.set("n", "<C-k>", require("arrow.persist").next)
+  -- vim.keymap.set("n", "<C-s>", require("arrow.persist").toggle)
 end
 
 return M
