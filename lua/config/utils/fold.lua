7d6df872762b7abdf5408baed1a41400853bf950
Pheon-Dev <devpheon@gmail.com>
2024-03-25 08:48:31 +0300

    [~] fold config [+] updates Monday, 25 March 2024

--- lua/config/utils/fold.lua
+++ lua/config/utils/fold.lua
@@ -117,16 +117,13 @@ M.config = function()
   end
 
   ufo.setup({
-    fold_virt_text_handler = handler,
+    --[[ fold_virt_text_handler = handler,
     open_fold_hl_timeout = 150,
     close_fold_kinds_for_ft = { "imports", "comment" },
     preview = {
       win_config = {
         border = "rounded",
-        --[[ border = { "", "─", "", "", "", "─", "", "" }, ]]
-        -- winhighlight = "Normal:PmenuSel",
         winblend = 0,
-        --[[ maxheight = 20 ]]
       },
       mappings = {
         scrollU = "<C-u>",
@@ -136,7 +133,7 @@ M.config = function()
     provider_selector = function(bufnr, filetype, buftype)
       return ftMap[filetype] or custom_selector
     end,
-    enable_get_fold_virt_text = true,
+    enable_get_fold_virt_text = true, ]]
   })
 
   -- buffer scope handler
