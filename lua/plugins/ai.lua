c37c25a95118682b8d76c57ca6469c76c400f669
Pheon-Dev <devpheon@gmail.com>
2024-04-18 09:19:16 +0300

    [+] NeoCodeium

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -8,6 +8,33 @@ local M = {
     event = default_event,
     enabled = enable.codeium,
   },
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    enabled = false,
+    config = function()
+      local neocodeium = require("neocodeium")
+      neocodeium.setup()
+      vim.keymap.set("i", "<A-c>", function()
+        require("neocodeium").accept()
+      end)
+      vim.keymap.set("i", "<A-w>", function()
+        require("neocodeium").accept_word()
+      end)
+      vim.keymap.set("i", "<A-a>", function()
+        require("neocodeium").accept_line()
+      end)
+      vim.keymap.set("i", "<A-e>", function()
+        require("neocodeium").cycle_or_complete()
+      end)
+      vim.keymap.set("i", "<A-r>", function()
+        require("neocodeium").cycle_or_complete(-1)
+      end)
+      vim.keymap.set("i", "<A-c>", function()
+        require("neocodeium").clear()
+      end)
+    end,
+  },
 }
 
 return M
