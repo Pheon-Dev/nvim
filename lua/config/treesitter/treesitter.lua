b274009bb24c4fb87cd46a7d23924bfbefd6a6c4
Pheon-Dev <devpheon@gmail.com>
2024-07-08 17:03:24 +0300

    [!] rust higlighting error

--- lua/config/treesitter/treesitter.lua
+++ lua/config/treesitter/treesitter.lua
@@ -41,6 +41,21 @@ M.config = function()
     },
     highlight = {
       enable = true,
+
+      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+      -- the name of the parser)
+      -- list of language that will be disabled
+      disable = { "c", "rust" },
+      -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+      --[[ disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end, ]]
+
       additional_vim_regex_highlighting = false,
     },
     -- matchup = {
