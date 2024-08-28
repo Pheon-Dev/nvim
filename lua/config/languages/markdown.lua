3f5ef6221666e32ecb366e1470ea0138ebd60564
Pheon-Dev <devpheon@gmail.com>
2024-08-28 09:22:18 +0300

    [+] markview dependencies

--- lua/config/languages/markdown.lua
+++ lua/config/languages/markdown.lua
@@ -1,6 +1,13 @@
 local M = {}
 
-M.dependencies = {}
+M.dependencies = {
+  -- You will not need this if you installed the
+  -- parsers manually
+  -- Or if the parsers are in your $RUNTIMEPATH
+  "nvim-treesitter/nvim-treesitter",
+
+  "nvim-tree/nvim-web-devicons",
+}
 
 M.lazy = false
 
