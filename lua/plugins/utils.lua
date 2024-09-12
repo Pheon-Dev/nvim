fad4e629254fc8a4b88bf3910b968e8e33f8dbda
Pheon-Dev <devpheon@gmail.com>
2024-09-12 15:31:11 +0300

    [+] sub

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -113,6 +113,36 @@ local M = {
     config = rip.config,
   },
   {
+    "gbprod/substitute.nvim",
+    enabled = false,
+    opts = {
+      on_substitute = nil,
+      yank_substituted_text = false,
+      preserve_cursor_position = false,
+      modifiers = nil,
+      highlight_substituted_text = {
+        enabled = true,
+        timer = 500,
+      },
+      range = {
+        prefix = "s",
+        prompt_current_text = false,
+        confirm = false,
+        complete_word = false,
+        subject = nil,
+        range = nil,
+        suffix = "",
+        auto_apply = false,
+        cursor_position = "end",
+      },
+      exchange = {
+        motion = false,
+        use_esc_to_cancel = true,
+        preserve_cursor_position = false,
+      },
+    },
+  },
+  {
     "dmtrKovalenko/caps-word.nvim",
     -- lazy = true,
     enabled = enable.caps,
